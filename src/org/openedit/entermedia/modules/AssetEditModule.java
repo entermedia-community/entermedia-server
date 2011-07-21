18cab537143af6cde15b92300a9648db2267c824
david <david@entermediasoftware.com>
2011-07-21 14:46:52 -0400

    now sets group on upload form

--- src/org/openedit/entermedia/modules/AssetEditModule.java
+++ src/org/openedit/entermedia/modules/AssetEditModule.java
@@ -713,6 +713,17 @@ public class AssetEditModule extends BaseMediaModule
 			}
 			readMetaData(inReq, archive,uploadprefixes[i], page, tracker, allids);
 		}
+		
+		//set the group view permissions if something was passed in
+		String groupid = inReq.getRequestParameter("viewgroup");
+		if(groupid != null)
+		{
+			for (Iterator iterator = tracker.iterator(); iterator.hasNext();)
+			{
+				Asset asset = (Asset) iterator.next();
+				archive.getAssetSecurityArchive().grantGroupViewAccess(archive, groupid, asset);
+			}
+		}
 
 		archive.saveAssets(tracker, inReq.getUser());
 
