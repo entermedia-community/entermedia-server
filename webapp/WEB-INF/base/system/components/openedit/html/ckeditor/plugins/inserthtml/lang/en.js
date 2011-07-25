84cbac951efd14d20e6a2e576402824eedeb0c37
cburkey <cburkey@entermediasoftware.com>
2011-07-25 15:55:19 -0400

    added link

--- webapp/WEB-INF/base/system/components/openedit/html/ckeditor/plugins/inserthtml/lang/en.js
+++
@@ -1,8 +0,0 @@
-﻿CKEDITOR.plugins.setLang("inserthtml","en",{
-  inserthtml:
-    {
-	 title:'Insert Html Code',
-	 commonTab:'Info',
-	 HelpInfo:'Enter any HTML below to insert at the location of the cursor<br> in the editor.'
-	}
-});
