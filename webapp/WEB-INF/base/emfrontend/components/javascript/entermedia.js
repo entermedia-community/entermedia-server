4f926d171643ee6b28c8c61d5c1b083f6d4df237
david <david@entermediasoftware.com>
2011-08-02 11:22:19 -0400

    reenabled link on results page cleaned up some velocity errors

--- webapp/WEB-INF/base/emfrontend/components/javascript/entermedia.js
+++ webapp/WEB-INF/base/emfrontend/components/javascript/entermedia.js
@@ -567,9 +567,6 @@ showajaxstatus = function()
 			var cell = jQuery(this);
 			var path = cell.attr("ajaxpath");
 			var hitid = cell.attr("linkid");
-			jQuery("#" + hitid).attr("onclick", "return false;");
-			//cell.load(path);
-			//cell.replaceWith(jQuery.ajax(path));
 			jQuery.get(path, {}, function(data) {
 				cell.replaceWith(data);
 			});
