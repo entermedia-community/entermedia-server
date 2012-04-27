00a24f0a633ff0883ce974cc06bd7b5672e68a74
cburkey <cburkey@entermediasoftware.com>
2012-04-27 09:54:28 -0400

    Added hotfolder

---
+++ src/org/openedit/entermedia/modules/HotFolderModule.java
@@ -0,0 +1,32 @@
+package org.openedit.entermedia.modules;
+
+import java.util.Collection;
+
+import org.openedit.entermedia.MediaArchive;
+import org.openedit.entermedia.scanner.HotFolderManager;
+
+import com.openedit.WebPageRequest;
+
+public class HotFolderModule extends BaseMediaModule
+{
+	public void loadHotFolders(WebPageRequest inReq)  throws Exception
+	{
+		MediaArchive archive = getMediaArchive(inReq);		
+		Collection folders = getHotFolderManager().loadFolders(archive);
+		inReq.putPageValue("folders", folders);
+	}
+	
+	public void saveHotFolders(WebPageRequest inReq) throws Exception
+	{
+		MediaArchive archive = getMediaArchive(inReq);
+		
+		getHotFolderManager().saveMounts(archive);
+		
+	}
+	protected HotFolderManager getHotFolderManager()
+	{
+		return (HotFolderManager)getModuleManager().getBean("hotFolderManager");
+	}
+
+	
+}
