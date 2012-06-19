f02dbbee990657473235aeb311b400f0d6d605c2
cburkey <cburkey@entermediasoftware.com>
2012-06-19 12:18:45 -0400

    Added lookup creation

--- webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/import/scripts/ImportCsvFile.groovy
+++ webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/import/scripts/ImportCsvFile.groovy
@@ -7,16 +7,12 @@ class CsvImporter extends BaseImporter
 	/**
 	 * This is an example of making a field lower case
 	 */
-	/*
 	protected void addProperties(Searcher inSearcher, Row inRow, Data inData)
 	{
-
-		String code = inRow.get("projectcode");
-		code = code.toLowerCase();
-		inRow.set("projectcode",code);
 		super.addProperties(inSearcher, inRow, inData);
+		//createLookUp(inSearcher.getCatalogId(),inData,"Division","val_divisions");
 	}
-	*/
+
 }
 
 
