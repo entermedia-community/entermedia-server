cbf361ba2f0134cf8c6826c80f75e42ed09b9d77
cburkey <cburkey@entermediasoftware.com>
2012-06-27 16:44:08 -0400

    Fix logic

--- webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/import/scripts/BaseImporter.groovy
+++ webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/import/scripts/BaseImporter.groovy
@@ -116,7 +116,7 @@ public class BaseImporter extends EnterMediaObject
 			List valueids = new ArrayList();
 			for (String val: values)
 			{
-				String id = PathUtilities.makeId(val).toLowerCase();
+				String id = PathUtilities.extractId(val,true);
 				Data data = datavalues.get(id);
 				if( data == null )
 				{
@@ -145,7 +145,7 @@ public class BaseImporter extends EnterMediaObject
 		{
 			datavalues = new HashMap();
 			getLookUps().put( inField, datavalues);
-			String id = PathUtilities.makeId(inField);
+			String id = PathUtilities.extractId(inField,true);
 			PropertyDetails details = getSearcher().getPropertyDetails()
 			PropertyDetail	detail = details.getDetail(id);
 			//if( detail.getL
@@ -179,7 +179,7 @@ public class BaseImporter extends EnterMediaObject
 			if( data == null )
 			{
 				//create it
-				String id = PathUtilities.makeId(value);
+				String id = PathUtilities.extractId(value,true);
 				Searcher searcher = getSearcherManager().getSearcher(getSearcher().getCatalogId(), inTable);
 				data = searcher.searchById(id);
 				if( data == null )
@@ -203,7 +203,7 @@ public class BaseImporter extends EnterMediaObject
 		{
 			String header = (String)iterator.next();
 			//String header = inHeaders[i];
-			String id = PathUtilities.makeId(header);
+			String id = PathUtilities.extractId(header,true);
 			PropertyDetail detail = details.getDetail(id);
 			if( detail == null )
 			{
@@ -226,7 +226,7 @@ public class BaseImporter extends EnterMediaObject
 		{
 			String val = inRow.getData(i);
 			String header = inRow.getHeader().getColumn(i);
-			String headerid = PathUtilities.makeId(header);
+			String headerid = PathUtilities.extractId(header,true);
 
 			val = URLUtilities.xmlEscape(val);
 			if("sourcepath".equals(header))
