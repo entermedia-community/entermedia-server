aeb8d9d985123dac21981eef20488878b3613bc8
cburkey <cburkey@entermediasoftware.com>
2012-08-06 15:02:40 -0400

    Added examples using objects

---
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/assets/model/AssetTypeManager.groovy
@@ -0,0 +1,66 @@
+package assets.model;
+
+import org.openedit.data.Searcher
+import org.openedit.Data
+import org.openedit.data.Searcher
+import org.openedit.entermedia.*
+import org.openedit.entermedia.creator.*
+import org.openedit.entermedia.edit.*
+import org.openedit.entermedia.episode.*
+import org.openedit.entermedia.modules.*
+import org.openedit.entermedia.search.AssetSearcher
+import org.openedit.xml.*
+
+import com.openedit.entermedia.scripts.EnterMediaObject;
+import com.openedit.hittracker.*
+import com.openedit.page.*
+import com.openedit.util.*
+
+import conversions.*
+
+public class AssetTypeManager extends EnterMediaObject 
+{
+	public void resetTypes() 
+	{
+		MediaArchive mediaarchive = (MediaArchive)context.getPageValue("mediaarchive");//Search for all files looking for videos
+
+		AssetSearcher searcher = mediaarchive.getAssetSearcher();
+
+		Searcher typesearcher = mediaarchive.getSearcher("assettype");
+
+		HitTracker types = typesearcher.getAllHits();
+		HashMap typemap = new HashMap();
+		types.each{
+			String extentions = it.extensions;
+			String type = it.id;
+			if(extentions != null){
+				String[] splits = extentions.split(" ");
+				splits.each{
+					typemap.put(it, type)
+				}
+			}
+		}
+		HitTracker allassets  = searcher.getAllHits();
+		List tosave = new ArrayList();
+		for (Data hit in allassets)
+		{
+			String fileformat = hit.fileformat;
+			String currentassettype = hit.assettype;
+			String assettype = typemap.get(fileformat);
+			if(assettype == null){
+				assettype="none";
+			}
+			if(!assettype.equals(currentassettype)){
+				Asset real = mediaarchive.getAsset(hit.id);
+				real.setProperty("assettype", assettype);
+				tosave.add(real);
+			}
+			if(tosave.size() == 100){
+				searcher.saveAllData(tosave, context.getUser());
+				tosave.clear();
+			}
+
+		}
+		searcher.saveAllData(tosave, context.getUser());
+	}
+}
--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/assetsimportedcustom.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/assetsimportedcustom.groovy
@@ -1,83 +1,22 @@
 package importing;
 
-import org.openedit.data.Searcher
-import org.entermedia.email.PostMail
-import org.entermedia.email.TemplateWebEmail
-import org.openedit.Data
-import org.openedit.data.Searcher
-import org.openedit.entermedia.*
-import org.openedit.entermedia.creator.*
-import org.openedit.entermedia.edit.*
-import org.openedit.entermedia.episode.*
-import org.openedit.entermedia.modules.*
-import org.openedit.entermedia.util.*
-import org.openedit.xml.*
+import assets.model.AssetTypeManager
+import assets.model.EmailNotifier;
 
-import com.openedit.WebPageRequest
-import com.openedit.hittracker.*
-import com.openedit.page.*
-import com.openedit.users.UserManager
-import com.openedit.util.*
+import com.openedit.page.manage.*;
 
-
-
-
-protected void sendEmail(WebPageRequest context, String email, String templatePage){
-	Page template = pageManager.getPage(templatePage);
-	WebPageRequest newcontext = context.copy(template);
-	TemplateWebEmail mailer = getMail();
-	mailer.loadSettings(newcontext);
-	mailer.setMailTemplatePath(templatePage);
-	mailer.setRecipientsFromCommas(email);
-	mailer.setSubject("Asset Upload Notification");
-	mailer.send();
-}
-
-protected TemplateWebEmail getMail() {
-	PostMail mail = (PostMail)mediaarchive.getModuleManager().getBean( "postMail");
-	return mail.getTemplateWebEmail();
-}
-
-
-
-
-
-
-public void emailOnImport() 
+public void setAssetTypes()
 {
-	log.info("Made it OK");
-	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
-	Searcher assetsearcher = mediaArchive.getAssetSearcher();
-	SearchQuery q = assetsearcher.createSearchQuery();
-	String ids = context.getRequestParameter("assetids");
-	if( ids == null)
-	{
-		return;
-	}
-	String assetids = ids.replace(","," ");
-	q.addOrsGroup( "id", assetids );
-	HitTracker assets = assetsearcher.search(q);
-	context.putPageValue("assets", assets);
-
-	Data setting = mediaarchive.getCatalogSetting("events_notify_app");
-	String appid = setting.get("value");
-		
-	def url = "/${appid}/components/notification/assetsimportedcustom.html"
-	
-	//get password and login
-	UserManager userManager = mediaArchive.getModuleManager().getBean("userManager");
-	
-	HitTracker hits = userManager.getUsersInGroup("uploadnotification");
-	
-	hits.each{
-		String email = it.email;
-		sendEmail(context, email, url);
-	}
-	
-	
-	
-
+	AssetTypeManager manager = new AssetTypeManager();
+	manager.context = context;
+	manager.resetTypes();
 }
-//Comment this in to enable emailing on upload example
-//emailOnImport();
+public void sendEmail()
+{
+	EmailNotifier emailer = new EmailNotifier();
+	emailer.context = context;
+	emailer.emailOnImport();
+}
 
+setAssetTypes();
+sendEmail();
