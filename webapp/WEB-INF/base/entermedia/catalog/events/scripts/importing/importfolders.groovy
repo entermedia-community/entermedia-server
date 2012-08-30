293be97996f0edeabb36f4bf8276333b9f10e833
cburkey <cburkey@entermediasoftware.com>
2012-08-30 10:31:34 -0400

    Added git check

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/importfolders.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/importfolders.groovy
@@ -1,10 +1,13 @@
 package importing;
 
-import org.openedit.entermedia.Asset 
 import org.openedit.entermedia.MediaArchive 
 import org.openedit.*;
 
 import com.openedit.hittracker.*;
+import com.openedit.page.Page
+import com.openedit.util.Exec
+import com.openedit.util.ExecResult
+
 import org.openedit.entermedia.creator.*;
 
 import java.util.Iterator;
@@ -23,6 +26,28 @@ public void init()
 	for(Iterator iterator = hits.iterator(); iterator.hasNext();)
 	{
 		Data folder = (Data)iterator.next();
+		String base = "/WEB-INF/data/" + archive.getCatalogId() + "/originals";
+		String name = folder.get("subfolder");
+		String path = base + "/" + name ;
+		Page page = pageManager.getPage(path + "/.git");
+		if( page.exists() )
+		{
+			Exec exec = moduleManager.getBean("exec");
+			List commands = new ArrayList();
+			commands.add("--git-dir");
+			commands.add(page.getContentItem().getAbsolutePath());
+			commands.add("pull");
+			ExecResult result = exec.runExec("git",commands);
+			if( result.isRunOk() )
+			{
+				log.info("pulled from git "  + path );
+			}
+			else
+			{
+				log.error("Could not pull from "  + path );
+			}		
+		}
+		
 		manager.importHotFolder(archive,folder);
 	}
 	
