d613ff4ad924aa19a00bc8cd6c7966c0827c1335
Ian Miller <ian@ijsolutions.ca>
2012-09-18 14:45:32 -0400

    Fixed a silly NPE

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/profile/checkprofile.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/profile/checkprofile.groovy
@@ -4,6 +4,10 @@ import org.openedit.Data
 import org.openedit.data.Searcher
 import org.openedit.entermedia.MediaArchive
 
+user = context.getUser();
+if(user == null){
+	return;
+}
 MediaArchive mediaarchive = context.getPageValue("mediaarchive");
 Searcher profilesearcher = mediaarchive.getSearcherManager().getSearcher(mediaarchive.getCatalogId(), "profile");
 Data profile = profilesearcher.searchById(context.getUser().getId());
