6e765b6db6ef52ebc605270d96f5949464c3d679
Ian Miller <ian@ijsolutions.ca>
2012-10-01 20:30:56 -0400

    Profile Searcher

---
+++ src/org/entermedia/profile/UserProfileSearcher.java
@@ -0,0 +1,61 @@
+package org.entermedia.profile;
+
+import org.openedit.Data;
+import org.openedit.data.Searcher;
+import org.openedit.data.XmlFileSearcher;
+import org.openedit.profile.UserProfile;
+
+import com.openedit.users.User;
+import com.openedit.users.UserManager;
+import com.openedit.users.authenticate.PasswordGenerator;
+
+public class UserProfileSearcher extends XmlFileSearcher {
+
+	public UserManager getUserManager() {
+		return fieldUserManager;
+	}
+
+	public void setUserManager(UserManager inUserManager) {
+		fieldUserManager = inUserManager;
+	}
+
+	protected UserManager fieldUserManager;
+	
+	@Override
+	public Data createNewData() {
+		UserProfile userProfile = (UserProfile) getModuleManager().getBean("userProfile");
+		userProfile.setCatalogId(getCatalogId());
+		User current = getUserManager().createUser(null, new PasswordGenerator().generate());
+
+		userProfile.setUser(current);
+		userProfile.setProperty("userid", current.getId());
+		return userProfile;
+	}
+
+	@Override
+	public Object searchById(String inId) {
+		
+		UserProfile search =  (UserProfile)super.searchById(inId);
+		String userid = search.getUserId();
+		User user = getUserManager().getUser(userid);
+		search.setUser(user);
+		return search;
+	}
+
+	@Override
+	public void saveData(Data inData, User inUser) {
+		UserProfile profile= (UserProfile)inData;
+		if(profile.getUser() == null){
+			User current = getUserManager().createUser(null, new PasswordGenerator().generate());
+			profile.setUser(current);
+			profile.setProperty("userid", current.getId());
+		}
+		Searcher usersearcher = getSearcherManager().getSearcher("system", "user");
+		usersearcher.saveData(profile.getUser(), inUser);
+		
+		// TODO Auto-generated method stub
+		super.saveData(inData, inUser);
+		
+	}
+	
+}
