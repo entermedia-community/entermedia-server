ba7d931f56f0cd0fb95189c8176c3420c92ed8c5
cburkey <cburkey@entermediasoftware.com>
2012-10-04 10:23:52 -0400

    Try full page extract first

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/conversions/creators/exiftoolthumbCreator.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/conversions/creators/exiftoolthumbCreator.groovy
@@ -31,24 +31,30 @@ public class exiftoolthumbCreator extends BaseImageCreator
 		Page input = inArchive.findOriginalMediaByType("image",inAsset);
 		if( input != null)
 		{
-			List command = new ArrayList();
+			new File( inOut.getContentItem().getAbsolutePath() ).getParentFile().mkdirs();
+			List base = new ArrayList();
 			//command.add("-b");
 			//command.add("-ThumbnailImage");
-			command.add(input.getContentItem().getAbsolutePath());
+			base.add(input.getContentItem().getAbsolutePath());
 			//command.add("-o");
-			command.add(inOut.getContentItem().getAbsolutePath());
-			
-			new File( inOut.getContentItem().getAbsolutePath() ).getParentFile().mkdirs();
-			//FileInputStream out = new FileOutputStream(inOut.getContentItem().getAbsolutePath());
+			base.add(inOut.getContentItem().getAbsolutePath());
+
+			List command = new ArrayList(base);			
+			command.add("PageImage");
 			ExecResult done = getExec().runExec("exiftoolthumb",command);
-			
+			if(inOut.length() == 0)
+			{
+				command = new ArrayList(base);
+				command.add("ThumbnailImage");
+				done = getExec().runExec("exiftoolthumb",command);
+			}	
 			result.setOk(done.isRunOk());
 			
 		}
 		if(inOut.length() == 0){
 			inArchive.getPageManager().removePage(inOut);
 			result.setOk(false);
-			result.setError("no thumbnail found");
+			result.setError("no embeded thumbnail found in file");
 		}
 		return result;
 	}
