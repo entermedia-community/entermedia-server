294c7cbb0e33a436ae665c53e04830a7bbc3bcf8
cburkey <cburkey@entermediasoftware.com>
2012-11-28 16:26:53 -0500

    Upload script with macro language

---
+++ webapp/WEB-INF/base/emfrontend/components/upload/types/html5/calculatesourcepath.groovy
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import com.openedit.page.Page
+import com.openedit.util.Replacer;
+
+import org.openedit.data.Searcher
+import org.openedit.entermedia.Asset
+import org.openedit.entermedia.MediaArchive
+import org.openedit.*;
+
+import com.openedit.WebPageRequest;
+import com.openedit.hittracker.*;
+
+public void init()
+{
+		MediaArchive mediaArchive = context.getPageValue("mediaarchive");//Search for all files looking for videos
+		
+		String prefix ="";
+		String[] fields = context.getRequestParameters("field");
+		Map vals = new HashMap();
+		vals.putAll(context.getPageMap());
+		
+		if( fields != null)
+		{
+			for (int i = 0; i < fields.length; i++)
+			{
+				String val = context.getRequestParameter(prefix + fields[i]+ ".value");
+				if( val != null)
+				{
+					vals.put(fields[i],val);
+				}
+			}
+		}
+		String sourcepath = context.findValue("destinationsourcepath");  //${division.uploadpath}/${user.userName}/${formateddate}
+		
+		Replacer replacer = new Replacer();
+		replacer.setSearcherManager(mediaArchive.getSearcherManager());
+		replacer.setDefaultCatalogId(mediaArchive.getCatalogId());
+		sourcepath = replacer.replace(sourcepath, vals);
+		if( sourcepath.startsWith("/") )
+		{
+			sourcepath = sourcepath.substring(1);
+		}
+		sourcepath = sourcepath.replace("//", "/"); //in case of missing data
+		
+		context.setRequestParameter("sourcepath",sourcepath);
+
+}
+
+init();
