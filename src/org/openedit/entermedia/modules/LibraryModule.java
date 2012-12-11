6906b65da1b9b4a2a9ae6ce0c834d4ec3369f06d
Ian <ian@silverstead.com>
2012-12-11 13:33:48 -0500

    Auto sync on drag and drop

--- src/org/openedit/entermedia/modules/LibraryModule.java
+++ src/org/openedit/entermedia/modules/LibraryModule.java
@@ -2,10 +2,14 @@ package org.openedit.entermedia.modules;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openedit.Data;
+import org.openedit.data.Searcher;
 import org.openedit.entermedia.Asset;
 import org.openedit.entermedia.MediaArchive;
 
 import com.openedit.WebPageRequest;
+import com.openedit.hittracker.HitTracker;
+import com.openedit.hittracker.SearchQuery;
 
 public class LibraryModule extends BaseMediaModule 
 {
@@ -21,8 +25,22 @@ public class LibraryModule extends BaseMediaModule
 		
 		if(asset != null && !asset.getLibraries().contains(libraryid)){
 			asset.addLibrary(libraryid);
+			archive.saveAsset(asset, inReq.getUser());
+
+
 		}
-		archive.saveAsset(asset, inReq.getUser());
+		Searcher libraryassets = archive.getSearcher("libraryassets");
+		SearchQuery query = libraryassets.createSearchQuery().append("libraryid", libraryid).append("assetid", assetid);
+		HitTracker hits = libraryassets.search(query);
+		if(hits.size() == 0){
+			Data newrow = libraryassets.createNewData();
+			newrow.setSourcePath(libraryid);
+			newrow.setProperty("libraryid", libraryid);
+			newrow.setProperty("assetid", assetid);
+			newrow.setId(libraryassets.nextId());
+			libraryassets.saveData(newrow, inReq.getUser());
+			
+		}
 		
 	}
 }
