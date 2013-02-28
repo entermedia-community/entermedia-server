5898f4e053e808c213b9ec8deb1781bdb8bbc4ed
Ian <ian@silverstead.com>
2013-02-28 10:51:45 -0500

    Random stuff

--- webapp/WEB-INF/base/emfrontend/views/modules/default/scripts/editor.groovy
+++ webapp/WEB-INF/base/emfrontend/views/modules/default/scripts/editor.groovy
@@ -1,17 +1,16 @@
 import org.openedit.Data
 import org.openedit.data.Searcher
 import org.openedit.data.SearcherManager
+import org.openedit.entermedia.MediaArchive
 
-import com.openedit.WebPageRequest
-import com.openedit.entermedia.scripts.EnterMediaObject
 import com.openedit.hittracker.HitTracker
 
-public class editor extends EnterMediaObject {
 
 
 
 	public void batchSave(){
-		SearcherManager sm = getModuleManager().getBean("searcherManager");
+		MediaArchive archive = context.getPageValue("mediaarchive");
+		SearcherManager sm = archive.getSearcherManager();
 		String catalogid = context.findValue("catalogid");
 		String searchtype = context.findValue("searchtype");
 		String sessionid = context.findValue("hitssessionid");
@@ -35,4 +34,4 @@ public class editor extends EnterMediaObject {
 	}
 
 
-}
+batchSave();
