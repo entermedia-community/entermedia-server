bfbabf2a98f0ab4f81291e579c4e3e96b2bbca19
smbest <smbest@hotmail.ca>
2013-06-21 10:59:46 -0400

    Changes to help support fatwire

--- webapp/WEB-INF/base/entermedia/src/publishing/publishers/fatwirepublisher.groovy
+++ webapp/WEB-INF/base/entermedia/src/publishing/publishers/fatwirepublisher.groovy
@@ -47,48 +47,55 @@ public class fatwirepublisher extends basepublisher implements Publisher
 			Data data = searcher.searchById(copyrightstatus);
 			usage = data.get("name");
 		}
-//		String fatwireId = inAsset.get("fatwireid");
-//		if (fatwireId != null)
-//		{
-//			result.setComplete(true);
-//			result.setErrorMessage("Not exporting asset, already exists on Fatwire");
-//			return result;
-//		}
-//		else
-//		{
-			//this does the actual publishing
-			Object fatwireManager = mediaArchive.getModuleManager().getBean( "fatwireManager");
-			try {
-				fatwireManager.setMediaArchive(mediaArchive);
-				Object assetBean = fatwireManager.pushAsset(inAsset, inUser, urlHome, usage, exportname, outputfile);
-				if (assetBean != null)
-				{
-					String newId = assetBean.getId();
-					String status = assetBean.getStatus();
-					log.info("response from publishing request to FatWire: newId ${newId} status ${status}");
-					inPublishRequest.setProperty("trackingnumber",newId);
-					result.setComplete(true);
-				}
-				else 
-				{
-					log.info("Error publishing asset: asset bean is NUll");
-					result.setComplete(true);
-					result.setErrorMessage("Error publishing to FatWire: unable to publish asset");
-				}
-			}
-			catch (IOException e)
+		//failsafe
+		if (exportname == null || urlHome == null || username == null || outputfile == null)
+		{
+			log.info("internal error: unable to publish to fatwire (exportname=${exportname} urlHome=${urlHome} username=${username} outputfile=${outputfile}");
+			
+			Iterator<String> itr = inPublishRequest.getProperties().keySet().iterator();
+			while (itr.hasNext())
 			{
-				log.error(e.getMessage(), e);
-				result.setComplete(true);
-				result.setErrorMessage(e.getMessage());
+				String key = itr.next();
+				String val = inPublishRequest.getProperties().get(key).toString();
+				System.out.println("property ${key} ${val}");
 			}
-			catch (Exception e)
+			
+			result.setComplete(true);
+			result.setErrorMessage("Error publishing to FatWire: variables have not been set");
+			return result;
+		}
+		//this does the actual publishing
+		Object fatwireManager = mediaArchive.getModuleManager().getBean( "fatwireManager");
+		try {
+			fatwireManager.setMediaArchive(mediaArchive);
+			Object assetBean = fatwireManager.pushAsset(inAsset, inUser, urlHome, usage, exportname, outputfile);
+			if (assetBean != null)
 			{
-				log.error(e.getMessage(), e);
+				String newId = assetBean.getId();
+				String status = assetBean.getStatus();
+				log.info("response from publishing request to FatWire: newId ${newId} status ${status}");
+				inPublishRequest.setProperty("trackingnumber",newId);
 				result.setComplete(true);
-				result.setErrorMessage(e.getMessage());
 			}
-//		}
+			else 
+			{
+				log.info("Error publishing asset: asset bean is NUll");
+				result.setComplete(true);
+				result.setErrorMessage("Error publishing to FatWire: unable to publish asset");
+			}
+		}
+		catch (IOException e)
+		{
+			log.error(e.getMessage(), e);
+			result.setComplete(true);
+			result.setErrorMessage(e.getMessage());
+		}
+		catch (Exception e)
+		{
+			log.error(e.getMessage(), e);
+			result.setComplete(true);
+			result.setErrorMessage(e.getMessage());
+		}
 		return result;
 	}
 }
