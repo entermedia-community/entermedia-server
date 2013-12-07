85fd15510068e8b53d2111c3698bdd909acd4488
Christopher Shanti Burkey <cburkey@entermediasoftware.com>
2013-12-07 11:47:01 -0500

    Clean up logs and formating

--- webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/export/generatecsv.groovy
+++ webapp/WEB-INF/base/emfrontend/views/settings/lists/datamanager/export/generatecsv.groovy
@@ -13,7 +13,7 @@ if(hits == null){
  String sessionid = context.getRequestParameter("hitssessionid");
  hits = context.getSessionValue(sessionid);
 }
-log.info("hits: " +hits);
+//log.info("hits: " +hits);
 searcherManager = context.getPageValue("searcherManager");
 searchtype = context.findValue("searchtype");
 catalogid = context.findValue("catalogid");
@@ -41,41 +41,47 @@ for (Iterator iterator = details.iterator(); iterator.hasNext();)
 	count++;
 }
 writer.writeNext(headers);
-	log.info("about to start: " + hits);
-for (Iterator iterator = hits.iterator(); iterator.hasNext();)
+	//log.info("about to start: " + hits);
+try
 {
-	hit =  iterator.next();
-	tracker = searcher.searchById(hit.get("id"));
 
-
-	nextrow = new String[details.size()];//make an extra spot for c
-	int fieldcount = 0;
-	for (Iterator detailiter = details.iterator(); detailiter.hasNext();)
+	for (Iterator iterator = hits.iterator(); iterator.hasNext();)
 	{
-		PropertyDetail detail = (PropertyDetail) detailiter.next();
-		String value = tracker.get(detail.getId());
-		//do special logic here
-		if(detail.isList() && friendly){
-			detail.get
-			Data remote  = searcherManager.getData( detail.getListCatalogId(),detail.getListId(), value);
-		
-				if(remote != null){
-				value= remote.getName();
-			}
+		hit =  iterator.next();
+		tracker = searcher.searchById(hit.get("id"));
+	
+		nextrow = new String[details.size()];//make an extra spot for c
+		int fieldcount = 0;
+		for (Iterator detailiter = details.iterator(); detailiter.hasNext();)
+		{
+			PropertyDetail detail = (PropertyDetail) detailiter.next();
+			String value = tracker.get(detail.getId());
+			//do special logic here
+			if(detail.isList() && friendly){
+				//detail.get
+				Data remote  = searcherManager.getData( detail.getListCatalogId(),detail.getListId(), value);
 			
-		}
+					if(remote != null){
+					value= remote.getName();
+				}
+				
+			}
 	
-
-		nextrow[fieldcount] = value;
-	
-		fieldcount++;
+			nextrow[fieldcount] = value;
+		
+			fieldcount++;
+		}	
+		writer.writeNext(nextrow);
 	}
 	
-	
-	
-	
-	writer.writeNext(nextrow);
 }
+catch( Exception ex)
+{
+	log.error("Could not process " + hit.getSourcePath() , ex);
+	writer.flush();
+	output.write("Could not process path: " + hit.getSourcePath() + " id:" + hit.getId() );
+}
+
 writer.close();
 
 //String finalout = output.toString();
