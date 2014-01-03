65449244cb57efb6e770b45da9f03102a35bc5ac
imiller <ian@netevolved.com>
2014-01-03 16:24:23 -0500

    New stuff

--- src/org/openedit/entermedia/modules/RegistrationModule.java
+++ src/org/openedit/entermedia/modules/RegistrationModule.java
@@ -26,51 +26,56 @@ import com.openedit.users.UserManager;
 import com.openedit.users.authenticate.PasswordGenerator;
 import com.openedit.util.RequestUtils;
 
-public class RegistrationModule extends BaseMediaModule {
+public class RegistrationModule extends BaseMediaModule
+{
 
-	
 	protected UserManager userManager;
 	protected SearcherManager fieldSearcherManager;
 
-	public SearcherManager getSearcherManager() {
+	public SearcherManager getSearcherManager()
+	{
 		return fieldSearcherManager;
 	}
 
-	public void setSearcherManager(SearcherManager inSearcherManager) {
+	public void setSearcherManager(SearcherManager inSearcherManager)
+	{
 		fieldSearcherManager = inSearcherManager;
 	}
 
-
-
 	private static final Log log = LogFactory.getLog(RegistrationModule.class);
-	
+
 	protected RequestUtils fieldRequestUtils;
 
-	public RequestUtils getRequestUtils() {
+	public RequestUtils getRequestUtils()
+	{
 		return fieldRequestUtils;
 	}
 
-	public void setRequestUtils(RequestUtils inRequestUtils) {
+	public void setRequestUtils(RequestUtils inRequestUtils)
+	{
 		fieldRequestUtils = inRequestUtils;
 	}
 
-	
-
 	static final String USERNAME_PARAMETER = "username";
 
-	public UserManager getUserManager() {
+	public UserManager getUserManager()
+	{
 		return userManager;
 	}
 
-	public void setUserManager(UserManager inUserManager) {
+	public void setUserManager(UserManager inUserManager)
+	{
 		userManager = inUserManager;
 	}
 
-	public void createGuestUser(WebPageRequest inReq) {
+	public void createGuestUser(WebPageRequest inReq)
+	{
 		User user = inReq.getUser();
-		if (user == null) {
+		if (user == null)
+		{
 			Group guest = getUserManager().getGroup("guest");
-			if (guest == null) {
+			if (guest == null)
+			{
 				getUserManager().createGroup("guest");
 			}
 
@@ -81,11 +86,12 @@ public class RegistrationModule extends BaseMediaModule {
 
 	}
 
-	public void checkUniqueEmail(WebPageRequest inReq) {
+	public void checkUniqueEmail(WebPageRequest inReq)
+	{
 
-		boolean allowduplicates = Boolean.parseBoolean(inReq
-				.findValue("allowduplicateemails"));
-		if (allowduplicates) {
+		boolean allowduplicates = Boolean.parseBoolean(inReq.findValue("allowduplicateemails"));
+		if (allowduplicates)
+		{
 			return;
 		}
 		String email = inReq.getRequestParameter("email.value");
@@ -93,59 +99,65 @@ public class RegistrationModule extends BaseMediaModule {
 		User user = getUserManager().getUserByEmail(email);
 
 		Map errors = new HashMap();
-		if (user != null) {
-			//errors.put("error-email-in-use", "This email address is in use");
-			//inReq.putPageValue("errors", errors);
+		if (user != null)
+		{
+			// errors.put("error-email-in-use", "This email address is in use");
+			// inReq.putPageValue("errors", errors);
 			inReq.putPageValue("emailinuse", true);
 			cancelAndForward(inReq);
 		}
 
 	}
-	
-	
-	public boolean checkCouponCode(WebPageRequest inReq){
-		if(Boolean.parseBoolean(inReq.findValue("requirecode"))){
-			
-		String catalogid = inReq.findValue("catalogid");
-		String couponcode = inReq.getRequestParameter("code.value");
-		
-		Map errors = new HashMap();
-		Searcher prepaidsearcher = getSearcherManager().getSearcher(catalogid, "prepaid");
-		Data prepaidcode = (Data) prepaidsearcher.searchById(couponcode);
-		if(prepaidcode == null){
-			prepaidcode = (Data) inReq.getPageValue("coupon");
+
+	public boolean checkCouponCode(WebPageRequest inReq)
+	{
+		if (Boolean.parseBoolean(inReq.findValue("requirecode")))
+		{
+
+			String catalogid = inReq.findValue("catalogid");
+			String couponcode = inReq.getRequestParameter("code.value");
+
+			Map errors = new HashMap();
+			Searcher prepaidsearcher = getSearcherManager().getSearcher(catalogid, "prepaid");
+			Data prepaidcode = (Data) prepaidsearcher.searchById(couponcode);
+			if (prepaidcode == null)
+			{
+				prepaidcode = (Data) inReq.getPageValue("coupon");
+			}
+
+			if (prepaidcode == null)
+			{
+
+				log.info("invalid code usage detected: " + couponcode);
+				errors.put("error-invalidcode", "This code is invalid");
+				inReq.putPageValue("errors", errors);
+				cancelAndForward(inReq);
+				return false;
+			}
+			boolean available = Boolean.parseBoolean(prepaidcode.get("available"));
+			if (!available)
+			{
+				log.info("attempted to use already used code: " + couponcode);
+				errors.put("error-codeused", "This code was already used");
+				inReq.putPageValue("errors", errors);
+				cancelAndForward(inReq);
+				return false;
+			}
+			log.info("processed code successfully" + couponcode);
+
+			inReq.putSessionValue("coupon", prepaidcode);
+			inReq.putPageValue("coupon", prepaidcode);
+
+			return true;
 		}
-		
-		if(prepaidcode == null){
-			
-			log.info("invalid code usage detected: " + couponcode);
-			errors.put("error-invalidcode", "This code is invalid");
-			inReq.putPageValue("errors", errors);
-			cancelAndForward(inReq);
-			return false;
-		}
-		boolean available  = Boolean.parseBoolean(prepaidcode.get("available"));
-		if(!available){
-			log.info("attempted to use already used code: " + couponcode);
-			errors.put("error-codeused", "This code was already used");
-			inReq.putPageValue("errors", errors);
-			cancelAndForward(inReq);
-			return false;
-		}
-		log.info("processed code successfully" + couponcode);
-		
-		inReq.putSessionValue("coupon", prepaidcode);
-		inReq.putPageValue("coupon", prepaidcode);
-		
-		return true;
-		} else{
+		else
+		{
 			return false;
 		}
 	}
 
-	
-
-	private void cancelAndForward(WebPageRequest inReq) {
+	private void cancelAndForward(WebPageRequest inReq)
+	{
 		String errorURL = inReq.findValue("errorURL");
 		inReq.setHasForwarded(true);
 		inReq.setCancelActions(true);
@@ -153,25 +165,27 @@ public class RegistrationModule extends BaseMediaModule {
 
 	}
 
-	public void registrationReceived(WebPageRequest inReq) throws Exception {
+	public void registrationReceived(WebPageRequest inReq) throws Exception
+	{
 		log.info("starting new registration");
-	
 
 		Map errors = new HashMap();
 		String email = inReq.getRequestParameter("email.value");
 		String password = inReq.getRequestParameter("password.value");
 		String password2 = inReq.getRequestParameter("password2.value");
-		if (password2 == null) {
+		if (password2 == null)
+		{
 			password2 = inReq.getRequestParameter("passwordmatch.value");
 		}
 		String errorURL = inReq.findValue("errorURL");
 
 		String[] fields = inReq.getRequestParameters("field");
 
-		if (fields == null) {
-			if (errorURL != null) {
-				errors.put("nodata",
-						"No data received by form - please try again.");
+		if (fields == null)
+		{
+			if (errorURL != null)
+			{
+				errors.put("nodata", "No data received by form - please try again.");
 				inReq.setHasForwarded(true);
 				inReq.putPageValue("errors", errors);
 				inReq.setCancelActions(true);
@@ -182,15 +196,18 @@ public class RegistrationModule extends BaseMediaModule {
 		}
 
 		Group guestgroup = getUserManager().getGroup("guest");
-		if (guestgroup == null) {
+		if (guestgroup == null)
+		{
 			guestgroup = getUserManager().createGroup("guest");
 		}
 
-		boolean generatepassword = Boolean.parseBoolean(inReq
-				.findValue("generatepassword"));
-		if (!generatepassword) {
-			if (password == null) {
-				if (errorURL != null) {
+		boolean generatepassword = Boolean.parseBoolean(inReq.findValue("generatepassword"));
+		if (!generatepassword)
+		{
+			if (password == null)
+			{
+				if (errorURL != null)
+				{
 					errors.put("password", "error-no-password");
 					inReq.setHasForwarded(true);
 					inReq.putPageValue("errors", errors);
@@ -202,8 +219,10 @@ public class RegistrationModule extends BaseMediaModule {
 
 			}
 
-			if (!password.equals(password2) || password.length() == 0) {
-				if (errorURL != null) {
+			if (!password.equals(password2) || password.length() == 0)
+			{
+				if (errorURL != null)
+				{
 					errors.put("password", "error-no-password-match");
 					inReq.setHasForwarded(true);
 					inReq.putPageValue("errors", errors);
@@ -219,16 +238,22 @@ public class RegistrationModule extends BaseMediaModule {
 		UserSearcher searcher = (UserSearcher) getSearcherManager().getSearcher("system", "user");
 
 		User current = inReq.getUser();
-		if (current != null && current.isVirtual()) {
+		if (current != null && current.isVirtual())
+		{
 			current.setVirtual(false);
 
-		} else {
+		}
+		else
+		{
 			current = getUserManager().createUser(null, password);
 		}
 
-		if (password != null && password.length() > 0) {
+		if (password != null && password.length() > 0)
+		{
 			current.setPassword(password);
-		} else if (generatepassword) {
+		}
+		else if (generatepassword)
+		{
 
 			current.setPassword(new PasswordGenerator().generate());// Integer.toString((int)(100000
 			// +
@@ -241,76 +266,78 @@ public class RegistrationModule extends BaseMediaModule {
 		current.setEmail(email);
 
 		current.addGroup(guestgroup);
-		
+
 		searcher.updateData(inReq, fields, current);
 		searcher.saveData(current, null);
-		
+
 		log.info("user id was" + current.getId());
 
 		current.remove("password");
 		current.remove("password2");
 
-		handleValidationCodes(inReq,  current);
+		handleValidationCodes(inReq, current);
 
-		boolean enable = Boolean.parseBoolean(inReq
-				.findValue("autoenable"));	
-		if(enable){
+		boolean enable = Boolean.parseBoolean(inReq.findValue("autoenable"));
+		if (enable)
+		{
 			current.setEnabled(true);
 		}
 		getUserManager().saveUser(current);
 		inReq.putPageValue("saved", "true");
 		inReq.putPageValue("newuser", current);
 		inReq.putPageValue("password", password);
-		//lets create a user profile now too.
+		// lets create a user profile now too.
 		MediaArchive archive = getMediaArchive(inReq);
-		UserProfileSearcher upsearcher= (UserProfileSearcher) archive.getSearcher("userprofile");
+		UserProfileSearcher upsearcher = (UserProfileSearcher) archive.getSearcher("userprofile");
 		UserProfile up = (UserProfile) upsearcher.createNewData();
 		up.setProperty("settingsgroup", "guest");
 		up.setUser(current);
 		up.setId(current.getId());
 		upsearcher.saveData(up, null);
 		inReq.putPageValue("data", up);
-		
-		
-		
+
 		Group notifygroup = getUserManager().getGroup("registration");
-		if (notifygroup == null) {
+		if (notifygroup == null)
+		{
 			notifygroup = getUserManager().createGroup("registration");
 		}
-		if (email != null) {
+		if (email != null)
+		{
 			inReq.setRequestParameter("to", email);
 
-		} else {
+		}
+		else
+		{
 			inReq.setRequestParameter("to", "dummy@ijsolutions.ca");
 		}
 		current.setProperty("creationdate", new Date().toString());
-		String subject = inReq.findValue("subjectprefix"); 
-		if(subject == null){
+		String subject = inReq.findValue("subjectprefix");
+		if (subject == null)
+		{
 			subject = "New Registration Received";
 		}
 		inReq.setRequestParameter("subject", subject);
 		inReq.putPageValue("registration", current);
-		
 
 		boolean logout = Boolean.parseBoolean(inReq.findValue("autologout"));
 
-		if (logout) {
+		if (logout)
+		{
 
 			inReq.removeSessionValue("user");
 		}
 
 		boolean login = Boolean.parseBoolean(inReq.findValue("autologin"));
 
-		if (login) {
+		if (login)
+		{
 
 			inReq.putSessionValue("user", current);// this logs in the user that
 													// just registered.
 		}
-		
-	
 
 	}
-	
+
 	public void checkPasswordMatch(WebPageRequest inReq) throws Exception
 	{
 		Map errors = new HashMap();
@@ -346,13 +373,13 @@ public class RegistrationModule extends BaseMediaModule {
 		}
 	}
 
-	protected void handleValidationCodes(WebPageRequest inReq,
-			 User inCurrent) {
-		boolean usecodes = Boolean.parseBoolean(inReq
-				.getPageProperty("usevalidationcodes"));
+	protected void handleValidationCodes(WebPageRequest inReq, User inCurrent)
+	{
+		boolean usecodes = Boolean.parseBoolean(inReq.getPageProperty("usevalidationcodes"));
 
 		String validationcode = inCurrent.get("validationcode");
-		if (validationcode == null || validationcode.length() == 0) {
+		if (validationcode == null || validationcode.length() == 0)
+		{
 			SecureRandom random = new SecureRandom();
 			validationcode =
 
@@ -360,52 +387,59 @@ public class RegistrationModule extends BaseMediaModule {
 			inCurrent.setProperty("validationcode", validationcode);
 
 		}
-		if (!usecodes) {
+		if (!usecodes)
+		{
 			return;
 
-		} else {
+		}
+		else
+		{
 			inCurrent.setEnabled(false);
 		}
 
 	}
 
-	public void validateCode(WebPageRequest inReq) {
+	public void validateCode(WebPageRequest inReq)
+	{
 		String userid = inReq.getRequestParameter("id");
 		String code = inReq.getRequestParameter("validationcode");
-		Boolean autoenable = Boolean.parseBoolean(inReq
-				.findValue("autoenable"));
+		Boolean autoenable = Boolean.parseBoolean(inReq.findValue("autoenable"));
 		User target = getUserManager().getUser(userid);
-		if (target != null) {
+		if (target != null)
+		{
 			String validationcode = target.get("validationcode");
-			if (code.equals(validationcode)) {
-				if (autoenable) {
+			if (code.equals(validationcode))
+			{
+				if (autoenable)
+				{
 					target.setEnabled(true);
 				}
 				target.setProperty("validationcomplete", "true");
 				inReq.putPageValue("validated", true);
 				getUserManager().saveUser(target);
 
-				boolean logout = Boolean.parseBoolean(inReq
-						.findValue("autologout"));
+				boolean logout = Boolean.parseBoolean(inReq.findValue("autologout"));
 
-				if (logout) {
+				if (logout)
+				{
 
 					inReq.removeSessionValue("user");
 				}
 
-				boolean login = Boolean.parseBoolean(inReq
-						.findValue("autologin"));
+				boolean login = Boolean.parseBoolean(inReq.findValue("autologin"));
 
-				if (login) {
+				if (login)
+				{
 
 					inReq.putSessionValue("user", target);// this logs in the
 															// user that just
 															// registered.
 				}
 				inReq.putPageValue("target", target);
-				
-				
-			} else {
+
+			}
+			else
+			{
 				inReq.putPageValue("validated", false);
 			}
 
@@ -413,36 +447,37 @@ public class RegistrationModule extends BaseMediaModule {
 
 	}
 
-	
-
-	
-	public boolean validateCodes(WebPageRequest inReq) {
-		boolean required = Boolean.parseBoolean(inReq
-				.getPageProperty("requirevalidationcode"));
-		if (!required) {
+	public boolean validateCodes(WebPageRequest inReq)
+	{
+		boolean required = Boolean.parseBoolean(inReq.getPageProperty("requirevalidationcode"));
+		if (!required)
+		{
 			log.info("not using validation codes");
 			return true;
 		}
 		Data validationCode = (Data) inReq.getSessionValue("registrationcode");
-		if (validationCode != null && validationCode.getName() == null) {
+		if (validationCode != null && validationCode.getName() == null)
+		{
 			validationCode.setName(validationCode.getId());
 		}
-		if (validationCode != null) {
+		if (validationCode != null)
+		{
 			inReq.putPageValue("registrationcode", validationCode);
 			return true;
 		}
 
 		String catalogid = inReq.findValue("catalogid");
 		String couponcode = inReq.getRequestParameter("registrationcode.value");
-		if (couponcode == null) {
+		if (couponcode == null)
+		{
 			return false;
 		}
-	
+
 		Map errors = new HashMap();
-		Searcher prepaidsearcher = getSearcherManager().getSearcher(catalogid,
-				"registrationcode");
+		Searcher prepaidsearcher = getSearcherManager().getSearcher(catalogid, "registrationcode");
 		Data code = (Data) prepaidsearcher.searchById(couponcode);
-		if (code == null || Boolean.parseBoolean(code.get("disabled"))) {
+		if (code == null || Boolean.parseBoolean(code.get("disabled")))
+		{
 			log.info("invalid code usage detected: " + couponcode);
 			errors.put("error-invalidcode", "This code is invalid");
 			inReq.putPageValue("errors", errors);
@@ -454,18 +489,25 @@ public class RegistrationModule extends BaseMediaModule {
 		current.setProperty("registrationcode", couponcode);
 		PropertyDetails details = prepaidsearcher.getPropertyDetails();
 
-		for (Iterator iterator = details.iterator(); iterator.hasNext();) {
+		for (Iterator iterator = details.iterator(); iterator.hasNext();)
+		{
 			PropertyDetail detail = (PropertyDetail) iterator.next();
 			String datatype = detail.getSearchType();
 			String value = code.get(detail.getId());
-			if (value != null) {
-				if ("team".equals(datatype) || "group".equals(datatype)) {
+			if (value != null)
+			{
+				if ("team".equals(datatype) || "group".equals(datatype))
+				{
 					Group group = getUserManager().getGroup(value);
-					if (group != null) {
+					if (group != null)
+					{
 						current.addGroup(group);
 					}
-				} else {
-					if (!"id".equals(detail.getId())) {
+				}
+				else
+				{
+					if (!"id".equals(detail.getId()))
+					{
 						current.setProperty(detail.getId(), value);
 					}
 				}
@@ -486,60 +528,64 @@ public class RegistrationModule extends BaseMediaModule {
 		log.info("detected coupon code: " + code.getId());
 		Searcher prepaidsearcher = getSearcherManager().getSearcher(inCatalogId, "prepaid");
 		code.setProperty("available", "false");
-		
+
 		String collegeid = code.get("college");
-		if (collegeid != null) {
+		if (collegeid != null)
+		{
 			Group group = getUserManager().getGroup(collegeid);
-			if (group == null) {
+			if (group == null)
+			{
 				group = getUserManager().createGroup(collegeid);
 				getUserManager().saveGroup(group);
 
 			}
-			if (!current.isInGroup(group)) {
+			if (!current.isInGroup(group))
+			{
 				current.addGroup(group);
 			}
 			current.setProperty("college", collegeid);
 
 		}
-		//if there is a class specified for this code, add them to it.
+		// if there is a class specified for this code, add them to it.
 		String classid = inReq.getRequestParameter("class.value");
-		if(classid != null){
-		
-				Group group = getUserManager().getGroup(classid + "_students");
-				if (group == null) {
-					group = getUserManager().createGroup(classid + "_students");
-					getUserManager().saveGroup(group);
+		if (classid != null)
+		{
 
-				}
-				if (!current.isInGroup(group)) {
+			Group group = getUserManager().getGroup(classid + "_students");
+			if (group == null)
+			{
+				group = getUserManager().createGroup(classid + "_students");
+				getUserManager().saveGroup(group);
+
+			}
+			if (!current.isInGroup(group))
+			{
 				current.addGroup(group);
 			}
-		
+
 		}
-		else{
+		else
+		{
 			String codeclass = code.get("class");
-			if(!codeclass.contains(" ")){
+			if (!codeclass.contains(" "))
+			{
 				Group group = getUserManager().getGroup(codeclass + "_students");
-				if (group == null) {
+				if (group == null)
+				{
 					group = getUserManager().createGroup(codeclass + "_students");
 					getUserManager().saveGroup(group);
 
 				}
-				if (!current.isInGroup(group)) {
+				if (!current.isInGroup(group))
+				{
 					current.addGroup(group);
 				}
 			}
 		}
-		
-		
-		
-		
-				
-		
+
 		code.setProperty("user", current.getId());
 		prepaidsearcher.saveData(code, inReq.getUser());
 		inReq.removeSessionValue("coupon");
 	}
-	
 
 }
