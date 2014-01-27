924f92e05793a969d5eed266ae65b47fa133490f
shawn <shawn@netevolved.com>
2014-01-27 16:22:06 -0500

    Temporary bug fix: Cookie.setHttpOnly(boolean) throwing exception

--- src/org/openedit/entermedia/modules/AdminModule.java
+++ src/org/openedit/entermedia/modules/AdminModule.java
@@ -585,7 +585,7 @@ public class AdminModule extends BaseModule
 				Cookie cookie = new Cookie(name, value);
 				cookie.setMaxAge(Integer.MAX_VALUE);
 				//Needs new servelet api jar
-				cookie.setHttpOnly(true);
+//				cookie.setHttpOnly(true);
 				cookie.setPath("/"); // http://www.unix.org.ua/orelly/java-ent/servlet/ch07_04.htm
 				res.addCookie(cookie);
 				inReq.putPageValue("entermediakey", value);
