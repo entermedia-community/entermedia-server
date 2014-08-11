a8f57eda69ef4009cf690b7155067cc28428a28c
Ian <ian@netevolved.com>
2014-08-11 16:22:16 -0400

    Added a new catalog setting to let you use filters to specify your default catalog tree structure for uploading.

--- src/com/openedit/webui/tree/BaseTreeRenderer.java
+++ src/com/openedit/webui/tree/BaseTreeRenderer.java
@@ -125,6 +125,7 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	public void selectNodes(Collection inNodes)
 	{
 		Set newselection = new HashSet();
+		if(inNodes != null){
 		for (Iterator iterator = inNodes.iterator(); iterator.hasNext();)
 		{
 			Object object = (Object) iterator.next();
@@ -134,6 +135,7 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 			{
 				expandNode(parent);
 			}
+		}
 		}
 		setSelectedNodes(newselection);
 		
