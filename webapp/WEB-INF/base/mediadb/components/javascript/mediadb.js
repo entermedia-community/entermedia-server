25f23f379b6e538d4f417fdeecaacb9b652bc6d7
Christopher Burkey <cburkey@openedit.org>
2014-10-24 14:23:55 -0400

    Added mediadb

---
+++ webapp/WEB-INF/base/mediadb/components/javascript/mediadb.js
@@ -0,0 +1,32 @@
+//Deal with networking code
+
+var MediaDb = function()
+{
+	//mediaDbUrl
+	
+	//entermediakey
+	
+	//getObject(url)
+
+	//getContent(url)
+
+	getLibraries()
+	
+	getCollections(libraryid)
+	
+	getCollectionAssets(collectionid)
+
+	searchAssets(query)
+	
+	searchTables(query)
+	
+	saveData(tablename,data)
+
+	
+}
+
+Asset
+
+
+Query
+
