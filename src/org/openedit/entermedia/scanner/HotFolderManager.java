5cb2426bef678f62c1cc34d64de383d4e685eafc
Christopher Burkey <cburkey@openedit.org>
2015-11-17 17:22:54 -0500

    interface added

---
+++ src/org/openedit/entermedia/scanner/HotFolderManager.java
@@ -0,0 +1,19 @@
+package org.openedit.entermedia.scanner;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.openedit.Data;
+import org.openedit.entermedia.MediaArchive;
+
+public interface HotFolderManager
+{
+	public Collection loadFolders(String inCatalogId);
+	
+	void deleteFolder(String inCatalogId, Data inExisting);
+
+	void saveFolder(String inCatalogId, Data inNewrow);
+
+	List<String> importHotFolder(MediaArchive inArchive, Data inFolder);
+	public void saveMounts(String inCatalogId);
+}
