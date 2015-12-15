71ad34022d2315c2f5e00356749c5f09f4aa017c
ian <ian@netevolved.com>
2015-12-14 20:08:37 -0500

    New code for Oauth login

---
+++ src/org/openedit/entermedia/modules/OauthModule.java
@@ -0,0 +1,213 @@
+package org.openedit.entermedia.modules;
+
+import java.util.Map;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthBearerClientRequest;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
+import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
+import org.apache.oltu.oauth2.common.OAuthProviderType;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.openedit.Data;
+import org.openedit.entermedia.MediaArchive;
+import org.openedit.entermedia.util.EmTokenResponse;
+import org.openedit.users.UserSearcher;
+
+import com.openedit.OpenEditException;
+import com.openedit.WebPageRequest;
+import com.openedit.users.User;
+import com.openedit.util.StringEncryption;
+
+public class OauthModule extends BaseMediaModule
+{	
+	
+	
+	protected StringEncryption fieldCookieEncryption;
+
+
+	public StringEncryption getCookieEncryption()
+	{
+		return fieldCookieEncryption;
+	}
+
+	public void setCookieEncryption(StringEncryption inCookieEncryption)
+	{
+		fieldCookieEncryption = inCookieEncryption;
+	}
+
+	public void redirectToHost(WebPageRequest inReq)
+	{
+		//http://yfrankfeng.blogspot.ca/2015/07/working-example-on-oauth2-spring.html
+		try
+		{
+
+			String provider = inReq.findValue("provider");
+			MediaArchive archive = getMediaArchive(inReq);
+			String appid = inReq.findValue("applicationid");
+			Data authinfo = archive.getData("oauthprovider", provider);
+			String redirect = inReq.findValue("siteroot") + "/" + appid + authinfo.get("redirecturi");
+			if ("google".equals(provider))
+			{
+
+				//.setClientId("1028053038230-v8g3isffne0b6d3vj8ceok61h2bfk9hg.apps.googleusercontent.com")
+				//.setRedirectURI("http://localhost:8080/googleauth.html")
+
+				OAuthClientRequest request = OAuthClientRequest.authorizationProvider(OAuthProviderType.GOOGLE).setClientId(authinfo.get("clientid")).setRedirectURI(redirect).setResponseType("code").setScope("https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid").buildQueryMessage();
+
+				String locationUri = request.getLocationUri();
+				inReq.redirect(locationUri);
+			}
+
+			if ("drupal".equals(provider))
+			{
+				//"https://devcondrupal.genieve.com/oauth2/authorize"
+				//"devemgenieve"
+				OAuthClientRequest request = OAuthClientRequest.authorizationLocation(authinfo.get("remoteroot") + "/oauth2/authorize").setClientId(authinfo.get("clientid")).setRedirectURI(redirect).setResponseType("code").setScope("openid email profile").setState("login").buildQueryMessage();
+
+				String locationUri = request.getLocationUri();
+				inReq.redirect(locationUri);
+
+			}
+
+		}
+		catch (Exception e)
+		{
+			throw new OpenEditException(e);
+		}
+	}
+
+	public void login(WebPageRequest inReq) throws Exception
+	{
+
+		String provider = inReq.findValue("provider");
+		String appid = inReq.findValue("applicationid");
+		MediaArchive archive = getMediaArchive(inReq);
+
+		if ("google".equals(provider))
+		{
+
+			Data authinfo = archive.getData("oauthprovider", provider);
+
+			OAuthAuthzResponse oar = OAuthAuthzResponse.oauthCodeAuthzResponse(inReq.getRequest());
+			String code = oar.getCode();
+			//GOOGLE
+
+			OAuthClientRequest request = OAuthClientRequest.tokenProvider(OAuthProviderType.GOOGLE).setGrantType(GrantType.AUTHORIZATION_CODE).setClientId(authinfo.get("clientid")).setClientSecret(authinfo.get("clientsecret")).setRedirectURI(inReq.findValue("siteroot") + "/" + appid + authinfo.get("redirecturi")).setCode(code).buildBodyMessage();
+
+			try
+			{
+
+				OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+				//Facebook is not fully compatible with OAuth 2.0 draft 10, access token response is
+				//application/x-www-form-urlencded, not json encoded so we use dedicated response class for that
+				//Own response class is an easy way to deal with oauth providers that introduce modifications to
+				//OAuth specification
+				EmTokenResponse oAuthResponse = oAuthClient.accessToken(request, EmTokenResponse.class);
+
+				// final OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request, "POST");
+				// final OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request);
+				String accessToken = oAuthResponse.getAccessToken();
+
+				OAuthClientRequest bearerClientRequest = new OAuthBearerClientRequest("https://www.googleapis.com/oauth2/v1/userinfo").setAccessToken(accessToken).buildQueryMessage();
+				OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, "GET", OAuthResourceResponse.class);
+				String userinfoJSON = resourceResponse.getBody();
+				JSONParser parser = new JSONParser();
+				
+				JSONObject data =  (JSONObject) parser.parse(userinfoJSON);
+				String email = (String)data.get("email");
+				String firstname = (String)data.get("given_name");
+				String lastname = (String)data.get("family_name");
+
+				handleLogin(inReq, "google-" + (String)data.get("id"), email, firstname, lastname,true, true);
+				
+			}
+			catch (Exception e)
+			{
+				throw new OpenEditException(e);
+			}
+		}
+
+		if ("drupal".equals(provider))
+		{
+			Data authinfo = archive.getData("oauthprovider", provider);
+
+			OAuthAuthzResponse oar = OAuthAuthzResponse.oauthCodeAuthzResponse(inReq.getRequest());
+			String code = oar.getCode();
+
+			String redirect = inReq.findValue("siteroot") + "/" + appid + authinfo.get("redirecturi");
+			OAuthClientRequest request = OAuthClientRequest.tokenLocation(authinfo.get("remoteroot") + "/oauth2/token").setGrantType(GrantType.AUTHORIZATION_CODE).setClientId(authinfo.get("clientid")).setClientSecret(authinfo.get("clientsecret")).setRedirectURI(redirect).setCode(code).buildBodyMessage();
+			OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+			EmTokenResponse oAuthResponse = oAuthClient.accessToken(request, EmTokenResponse.class);
+			// final OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request, "POST");
+			// final OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request);
+
+			String accessToken = oAuthResponse.getAccessToken();
+
+			OAuthClientRequest bearerClientRequest = new OAuthBearerClientRequest(authinfo.get("remoteroot") + "/oauth2/UserInfo").setAccessToken(accessToken).buildQueryMessage();
+
+			OAuthResourceResponse resourceResponse = oAuthClient.resource(bearerClientRequest, "GET", OAuthResourceResponse.class);
+			String userinfoJSON = resourceResponse.getBody();
+			JSONParser parser = new JSONParser();
+			
+			JSONObject data =  (JSONObject) parser.parse(userinfoJSON);
+			handleLogin(inReq, "drupal-" + (String)data.get("id"), (String)data.get("firstname"), (String)data.get("lastname"), (String)data.get("email"),true, true);
+
+		}
+
+	}
+
+	protected void handleLogin(WebPageRequest inReq, String username, String email, String firstname, String lastname, boolean matchOnEmail, boolean autocreate)
+	{
+		MediaArchive archive = getMediaArchive(inReq);
+		UserSearcher searcher = (UserSearcher) archive.getSearcher("user");
+
+		User target = (User) searcher.searchById(username);
+
+		if (target != null)
+		{
+			inReq.putSessionValue(searcher.getCatalogId() + "user", target);
+
+		}
+		if (matchOnEmail)
+		{
+			Data record = (Data) searcher.searchByField("email", email);
+
+			if (record != null)
+			{
+				target = (User) searcher.searchById(record.getId());
+				if (target != null)
+				{
+					inReq.putSessionValue(searcher.getCatalogId() + "user", target);
+
+				}
+			}
+		}
+
+		if (autocreate && target == null)
+		{
+			target = (User) searcher.createNewData();
+			target.setId(username);
+			target.setFirstName(firstname);
+			target.setLastName(lastname);
+			target.setEmail(email);
+			searcher.saveData(target, null);
+			inReq.putSessionValue(archive.getCatalogId() + "user", target);
+
+		}
+		if(target != null){
+			
+			String md5 = getCookieEncryption().getPasswordMd5(target.getPassword());
+			String value = target.getUserName() + "md542" + md5;
+			inReq.putPageValue("entermediakey", value);
+			inReq.putPageValue( "user", target);
+
+		}
+
+
+	}
+
+}
---
+++ src/org/openedit/entermedia/util/EmTokenResponse.java
@@ -0,0 +1,111 @@
+/**
+ *       Copyright 2010 Newcastle University
+ *
+ *          http://research.ncl.ac.uk/smart/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openedit.entermedia.util;
+
+import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.token.BasicOAuthToken;
+import org.apache.oltu.oauth2.common.token.OAuthToken;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.openedit.OpenEditException;
+
+/**
+ *
+ *
+ *
+ */
+public class EmTokenResponse extends OAuthAccessTokenResponse
+{
+
+	protected JSONObject fieldData;
+	
+	
+	public JSONObject getData()
+	{
+		return fieldData;
+	}
+
+	public void setData(JSONObject inData)
+	{
+		fieldData = inData;
+	}
+
+	public String getAccessToken()
+	{
+		return (String) getData().get("access_token");
+	}
+
+	public Long getExpiresIn()
+	{
+		Long value = (Long) getData().get("expires_in");
+		return value;
+	}
+
+	public String getRefreshToken()
+	{
+		return (String) getData().get("refresh_token");
+	}
+
+	public String getScope()
+	{
+		return (String) getData().get("scope");
+	}
+
+	public OAuthToken getOAuthToken()
+	{
+		return new BasicOAuthToken(getAccessToken(), getExpiresIn(), getRefreshToken(), getScope());
+	}
+
+	protected void setBody(String body)
+	{
+		this.body = body;
+		JSONParser parser = new JSONParser();
+		try{
+		this.fieldData = (JSONObject) parser.parse(body);
+		} catch (Exception e){
+			throw new OpenEditException(e);
+		}
+
+	}
+
+	protected void setContentType(String contentType)
+	{
+		this.contentType = contentType;
+	}
+
+	protected void setResponseCode(int code)
+	{
+		this.responseCode = code;
+	}
+	
+	
+	@Override
+	public String getParam(String inParam)
+	{
+		
+		return  (String) getData().get(inParam);
+	}
+	
+
+}
