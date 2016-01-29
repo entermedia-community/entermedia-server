0f03567b93160885d85d3cbc350614433cd1a145
ian <ian@netevolved.com>
2016-01-29 18:26:12 -0500

    Fixes for Elasticsearch and field mappings

---
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/data/importdatabase.groovy
@@ -0,0 +1,118 @@
+package data;
+
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.asset.util.CSVReader
+import org.entermediadb.asset.util.ImportFile
+import org.entermediadb.asset.util.Row
+import org.openedit.Data
+import org.openedit.data.*
+import org.openedit.page.Page
+import org.openedit.util.DateStorageUtil;
+import org.openedit.util.PathUtilities
+import org.openedit.xml.XmlFile
+
+public void init(){
+
+	MediaArchive mediaarchive = context.getPageValue("mediaarchive");
+	catalogid = context.findValue("catalogid");
+	SearcherManager searcherManager = context.getPageValue("searcherManager");
+	PropertyDetailsArchive archive = mediaarchive.getPropertyDetailsArchive();
+	List searchtypes = archive.getPageManager().getChildrenPaths("/WEB-INF/data/" + catalogid + "/dataexport/");
+	searchtypes.each{
+		
+		Page upload = mediaarchive.getPageManager().getPage(it);		
+		
+		String searchtype = upload.getName();		
+		searchtype = searchtype.substring(0, searchtype.length() - 4);
+		
+		String filepath = "/WEB-INF/data/" + catalogid + "/dataexport/fields/" + searchtype + ".xml";
+			
+		if(it.endsWith(".csv")) {
+			Searcher searcher = searcherManager.getSearcher(catalogid, searchtype);
+			PropertyDetails details = searcher.getPropertyDetails();
+			
+			
+			
+			Reader reader = upload.getReader();
+			ImportFile file = new ImportFile();
+			file.setParser(new CSVReader(reader, ',', '\"'));
+			file.read(reader);
+
+			Row trow = null;
+			ArrayList data = new ArrayList();
+			while( (trow = file.getNextRow()) != null )
+			{
+			
+				if(searchtype == "settingsgroup"){
+					continue;//need to deal with this one seperately
+				}
+				
+					
+				String id = trow.get("id");
+				Data newdata = searcher.searchById(id);
+				if(newdata == null){
+					newdata = searcher.createNewData();
+					newdata.setId(id);
+				}
+				data.add(newdata);
+				
+				for (Iterator iterator = file.getHeader().getHeaderNames().iterator(); iterator.hasNext();)
+				{
+					String header = (String)iterator.next();
+					String detailid = PathUtilities.extractId(header,true);
+					PropertyDetail detail = details.getDetail(detailid);
+					if(detail == null){
+						//see if we have a legacy field for this?
+						details.each {
+							String legacy = it.get("legacy");
+							if(legacy != null && legacy.equals(header)){
+								detail = it;
+							} 
+						}						
+					}
+					if(detail == null){
+						continue; // this should not happen if you run mergemappigns first
+					}
+										
+					
+					String value = trow.get(header);
+					
+					if(detail.isDate()){
+						try{
+							Date date = DateStorageUtil.getStorageUtil().parseFromStorage(value);
+							value = DateStorageUtil.getStorageUtil().formatForStorage(date);
+						} catch (Exception e){
+							value= null;
+						}						
+					}
+					newdata.setProperty(detail.getId(), value);
+					
+					
+				}
+				
+				
+				
+				if(data.size() > 100){
+					searcher.saveAllData(data, null);
+					data.clear();
+				}
+			}
+			
+			
+			searcher.saveAllData(data, null);
+			data.clear();
+			
+			
+			
+			
+			
+		}
+	}
+}
+
+
+
+
+
+
+init();
---
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/data/mergemappings.groovy
@@ -0,0 +1,160 @@
+package data;
+
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.asset.search.BaseAssetSearcher;
+import org.entermediadb.asset.util.CSVReader
+import org.entermediadb.asset.util.ImportFile
+import org.entermediadb.asset.util.Row
+import org.entermediadb.elasticsearch.searchers.BaseElasticSearcher
+import org.entermediadb.elasticsearch.searchers.ElasticAssetDataConnector;
+import org.openedit.Data
+import org.openedit.data.*
+import org.openedit.page.Page
+import org.openedit.util.DateStorageUtil;
+import org.openedit.util.PathUtilities
+import org.openedit.xml.XmlFile
+
+public void init(){
+
+	MediaArchive mediaarchive = context.getPageValue("mediaarchive");
+	catalogid = context.findValue("catalogid");
+	SearcherManager searcherManager = context.getPageValue("searcherManager");
+	PropertyDetailsArchive archive = mediaarchive.getPropertyDetailsArchive();
+	List searchtypes = archive.getPageManager().getChildrenPaths("/WEB-INF/data/" + catalogid + "/dataexport/");
+	searchtypes.each{		
+		if(it.endsWith(".csv")) {
+			boolean savedetails = false;
+			PropertyDetails olddetails = null;
+			Page upload = mediaarchive.getPageManager().getPage(it);
+			String searchtype = upload.getName();
+			searchtype = searchtype.substring(0, searchtype.length() - 4);
+	
+			String filepath = "/WEB-INF/data/" + catalogid + "/dataexport/fields/" + searchtype + ".xml";
+			XmlFile settings = archive.getXmlArchive().loadXmlFile(filepath); // checks time
+			if(settings.isExist()){
+				olddetails = new PropertyDetails();
+				olddetails.setInputFile(settings);
+				archive.setAllDetails(olddetails, searchtype, settings.getRoot());
+			}
+	
+			PropertyDetails currentdetails = archive.getPropertyDetails(searchtype);
+	
+			if(olddetails != null){
+				String beanname = olddetails.getBeanName();
+	
+				if(beanname  && (beanname.contains("xmlFile") || beanname.contains("data"))){
+					if(currentdetails != null && !"dataSearcher".equals(currentdetails.getBeanName())){
+						currentdetails.setBeanName("dataSearcher");
+						savedetails = true;
+					}	
+				}
+			}
+			
+			Reader reader = upload.getReader();
+			ImportFile file = new ImportFile();
+			file.setParser(new CSVReader(reader, ',', '\"'));
+			file.read(reader);
+
+			for (Iterator iterator = file.getHeader().getHeaderNames().iterator(); iterator.hasNext();)
+			{
+				String header = (String)iterator.next();
+				String detailid = PathUtilities.extractId(header,true);
+				PropertyDetail detail = currentdetails.getDetail(detailid);
+				
+				
+				if(detail == null){
+					//see if we have a legacy field for this- if so it'll get remapped during the import and we don't want to add it
+					currentdetails.each {
+						String legacy = it.get("legacy");
+						if(legacy != null && legacy.equals(header)){
+							detail = it;
+						}
+					}
+				}
+				
+				
+				
+				
+				if(detail == null && olddetails != null){
+					detail = olddetails.getDetail(detailid);
+					if(detail != null){
+						currentdetails.addDetail(detail);
+					}
+					savedetails = true;
+				}
+
+				if(detail == null){
+					detail = new PropertyDetail();
+					detail.setId(header);
+					detail.setText(header);
+					detail.setEditable(true);
+					detail.setIndex(true);
+					detail.setStored(true);
+					currentdetails.addDetail(detail);
+					savedetails = true;
+				}
+				
+				if(detail != null){
+					
+					if(detail.getId().contains(".")){
+						String newid = detail.getId().replace('.', '_');
+						detail.setProperty("legacy", detail.getId());
+						detail.setId(newid);
+						savedetails = true;
+					}
+				}
+				
+
+			}
+
+			if(savedetails){
+				mediaarchive.getPropertyDetailsArchive().savePropertyDetails(currentdetails, searchtype, context.getUser());
+
+			}
+
+		}
+	}
+	
+	
+	
+	List allsearchtypes = mediaarchive.getPropertyDetailsArchive().listSearchTypes();
+	allsearchtypes.each{
+		//write all these mappings - then we can check the error page.
+		Searcher searcher  = mediaarchive.getSearcher(it);
+		
+		if (searcher instanceof BaseElasticSearcher)
+		{
+			BaseElasticSearcher new_name = (BaseElasticSearcher) searcher;
+			if(new_name.getAllHits().size() > 0){
+				new_name.putMappings(null);
+			}
+			
+		}
+		
+		if (searcher instanceof BaseAssetSearcher)
+		{
+			BaseAssetSearcher new_name = (BaseAssetSearcher) searcher;
+			if(new_name.getAllHits().size() > 0){
+				ElasticAssetDataConnector con = (ElasticAssetDataConnector) new_name.getDataConnector();
+				con.putMappings(null);
+			}
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+		init();
