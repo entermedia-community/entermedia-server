331b101d46abba5f5654646df39d21d8ddcb923e
cristobalmejia <cristobal@entermediasoftware.com>
2016-02-17 12:00:55 -0600

    Script running only in one HotFolder, also analysiyng all assets every time

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/assetsclear.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/assetsclear.groovy
@@ -28,90 +28,86 @@ public void init()
 		List paths = pageManager.getChildrenPaths(path);
 		if( paths.size() == 0)
 		{
-			log.info("Found hot folder with no files, canceled delete request " + path);
-			return;
+			continue;
 		}
-	}
+		log.info("Analysing Hot folder " + path);
 	
-	List children = pageManager.getChildrenPaths("/WEB-INF/data/" + archive.getCatalogId() + "/originals");
-	if(children.size() == 0 )
-	{
-		log.info("No originals found. Skipping clear");
-		return;	
-	}
-
-	AssetSearcher searcher = archive.getAssetSearcher();
-	SearchQuery q = searcher.createSearchQuery();
-	HitTracker assets = null;
-	String sourcepath = context.getRequestParameter("sourcepath");
-	if(sourcepath == null)
-	{
-		q = searcher.createSearchQuery().append("category", "index");
-		q.addNot("editstatus","7");
-	}
-	else
-	{
-		q.addStartsWith("sourcepath", sourcepath);
-	}
-	q.addSortBy("sourcepath");
-	assets = searcher.search(q);
-	assets.setHitsPerPage(1000);
-	int removed = 0;
-	List tosave = new ArrayList();
-	int existed = 0;	
-	for(Object obj: assets)
-	{
-		Data hit = (Data)obj;
-	
-		String assetsource = hit.getSourcePath();
-		String pathToOriginal = "/WEB-INF/data" + archive.getCatalogHome() + "/originals/" + assetsource;
-		
-		if(!pageManager.getRepository().doesExist(pathToOriginal) )
+		AssetSearcher searcher = archive.getAssetSearcher();
+		SearchQuery q = searcher.createSearchQuery();
+		HitTracker assets = null;
+		//String sourcepath = context.getRequestParameter("sourcepath");
+		String sourcepath =  name;
+		if(sourcepath == null)
 		{
-			Asset asset = archive.getAssetBySourcePath(assetsource);
-			if( asset == null)
-			{
-				log.info("invalid asset " + path);
-				continue;
-			}
-
-			if(asset.isFolder() && asset.getPrimaryFile() != null)
-			{
-				pathToOriginal = pathToOriginal + "/" + asset.getPrimaryFile();
-				if( pageManager.getRepository().doesExist(pathToOriginal) )
-				{
-					existed++;
-					continue; //never mind, it is here
-				}
-			}
-			removed++;
-			//archive.removeGeneratedImages(asset);
-           if( asset.get("editstatus") != "7" )
-           {
-			   asset.setProperty("editstatus", "7");
-			   tosave.add(asset);
-           }
+			q = searcher.createSearchQuery().append("category", "index");
+			q.addNot("editstatus","7");
 		}
 		else
 		{
-			existed++;
+			q.addStartsWith("sourcepath", sourcepath);
+		}
+
+		q.addSortBy("sourcepath");
+		assets = searcher.search(q);
+		assets.setHitsPerPage(1000);
+		int removed = 0;
+		List tosave = new ArrayList();
+		int existed = 0;	
+		for(Object obj: assets)
+		{
+
+			Data hit = (Data)obj;
+	
+			String assetsource = hit.getSourcePath();
+			String pathToOriginal = "/WEB-INF/data" + archive.getCatalogHome() + "/originals/" + assetsource;
+		
+			if(!pageManager.getRepository().doesExist(pathToOriginal) )
+			{
+				Asset asset = archive.getAssetBySourcePath(assetsource);
+				if( asset == null)
+				{
+					log.info("invalid asset " + path);
+					continue;
+				}
+
+
+				if(asset.isFolder() && asset.getPrimaryFile() != null)
+				{
+					pathToOriginal = pathToOriginal + "/" + asset.getPrimaryFile();
+					if( pageManager.getRepository().doesExist(pathToOriginal) )
+					{
+						existed++;
+						continue; //never mind, it is here
+					}
+				}
+				removed++;
+				//archive.removeGeneratedImages(asset);
+        	    if( asset.get("editstatus") != "7" )
+         	    {
+				   asset.setProperty("editstatus", "7");
+				   tosave.add(asset);
+           	    }
+			}
+			else
+			{
+				existed++;
 //            if( asset.get("editstatus") != "7" )
 //            {
 //			   asset.setProperty("editstatus", "6"); //restore files
 //			   tosave.add(asset);
 //            }
+			}
+			if( tosave.size() == 100 )
+			{
+				log.info("removed " + removed + " found " + existed);
+				archive.saveAssets(tosave);
+				tosave.clear();
+			}
 		}
-		if( tosave.size() == 100 )
-		{
-			log.info("removed " + removed + " found " + existed);
-			archive.saveAssets(tosave);
-			tosave.clear();
-		}
-	}
-	archive.saveAssets(tosave);
-	tosave.clear();
-	log.info("removed " + removed + " found " + existed);
-	
+		archive.saveAssets(tosave);
+		tosave.clear();
+		log.info("removed " + removed + " found " + existed);
+ 	}	
 }
 
 
