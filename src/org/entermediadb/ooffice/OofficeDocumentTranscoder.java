c75f200d19582de14119069292c4c51a74861a89
ian <ian@netevolved.com>
2016-03-07 13:30:19 -0500

    New code

---
+++ src/org/entermediadb/ooffice/OofficeDocumentTranscoder.java
@@ -0,0 +1,117 @@
+package org.entermediadb.ooffice;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.entermediadb.asset.Asset;
+import org.entermediadb.asset.MediaArchive;
+import org.entermediadb.asset.convert.BaseTranscoder;
+import org.entermediadb.asset.convert.ConvertInstructions;
+import org.entermediadb.asset.convert.ConvertResult;
+import org.openedit.OpenEditException;
+import org.openedit.page.Page;
+import org.openedit.repository.ContentItem;
+import org.openedit.util.ExecResult;
+import org.openedit.util.PathUtilities;
+
+public class OofficeDocumentTranscoder extends BaseTranscoder
+{
+//	protected final def formats = ["doc","docx","rtf","ppt","pptx","wps","odt","html","xml","csv", "xls", "xlsx", "odp"];
+	private static final Log log = LogFactory.getLog(OofficeDocumentTranscoder.class);
+	
+	public synchronized ConvertResult convert(ConvertInstructions inStructions)
+	{
+		MediaArchive inArchive = inStructions.getMediaArchive();
+		Asset inAsset = inStructions.getAsset();
+		ContentItem inOut = inStructions.getOutputFile();
+		
+		ConvertResult result = new ConvertResult();
+		result.setOk(false);
+		
+		Page input = inArchive.findOriginalMediaByType("document",inAsset);
+		if( input == null )
+		{
+			return result;
+		}
+
+		List command = new ArrayList();
+		
+		command.add("-headless");
+		command.add("-nologo");
+		//command.add("-invisible");
+		command.add("-norestore");		
+		command.add("-nolockcheck");
+		
+		command.add("-convert-to");		
+		command.add("pdf:writer_pdf_Export");
+
+		command.add("-outdir");
+		
+		Page out = getPageManager().getPage(inOut.getPath());
+		String outfolder = out.getDirectory();
+		String dir = out.getDirectory();
+		dir = getPageManager().getPage(dir).getContentItem().getAbsolutePath();
+		//log.info("{$inOut} turns into ${dir}");
+		//String dir = inStructions.getOutputFile().getAbsolutePath();
+		//new File( dir ).mkdirs();
+		command.add(dir);
+		
+		command.add(input.getContentItem().getAbsolutePath());
+		
+		long timeout = inStructions.getConversionTimeout();
+		ExecResult done = getExec().runExec("soffice",command, timeout);
+		
+		result.setOk(done.isRunOk());
+		if( done.isRunOk() )
+		{
+			String newname = PathUtilities.extractPageName(input.getName()) + ".pdf";
+			
+			Page tmpfile = getPageManager().getPage(outfolder+ "/" + newname);
+			if( !tmpfile.exists())
+			{
+				throw new OpenEditException("OpenOffice did not create output file " + tmpfile);
+			}
+			Page output = getPageManager().getPage(inOut.getPath());
+			getPageManager().movePage(tmpfile, output);
+			log.info("Completed: " + input.getName());
+		}
+	    result.setOk(true);
+	    return result;
+	}
+
+//	public String populateOutputPath(MediaArchive inArchive, ConvertInstructions inStructions)
+//	{
+//		//we only generate PDF for now
+//		StringBuffer path = new StringBuffer();
+//
+//		//legacy for people who want to keep their images in the old location
+//		String prefix = inStructions.getProperty("pathprefix");
+//		if( prefix != null)
+//		{
+//			path.append(prefix);
+//		}
+//		else
+//		{
+//			path.append("/WEB-INF/data");
+//			path.append(inArchive.getCatalogHome());
+//			path.append("/generated/");
+//		}
+//		path.append(inStructions.getAssetSourcePath());
+//		path.append("/");
+//
+//		String postfix = inStructions.getProperty("pathpostfix");
+//		if( postfix != null)
+//		{
+//			path.append(postfix);
+//		}	
+//		inStructions.setOutputExtension("pdf");
+//		path.append("document." + inStructions.getOutputExtension());
+//
+//		inStructions.setOutputPath(path.toString());
+//		return path.toString();
+//	}
+
+}
---
+++ src/org/entermediadb/ooffice/OofficeTextExtractor.java
@@ -0,0 +1,91 @@
+package org.entermediadb.ooffice;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.entermediadb.asset.Asset;
+import org.entermediadb.asset.MediaArchive;
+import org.entermediadb.asset.convert.ConversionManager;
+import org.entermediadb.asset.convert.ConvertInstructions;
+import org.entermediadb.asset.scanner.MetadataExtractor;
+import org.entermediadb.asset.scanner.MetadataPdfExtractor;
+import org.openedit.repository.ContentItem;
+import org.openedit.util.PathUtilities;
+
+public class OofficeTextExtractor extends MetadataExtractor
+{
+	public static final Collection FORMATS = Arrays.asList(new String[] {"doc","docx","rtf","ppt","pptx","wps","odt","html","xml","csv", "xls", "xlsx"});
+
+	private static final Log log = LogFactory.getLog(OofficeTextExtractor.class);
+	protected MetadataPdfExtractor fieldMetadataPdfExtractor;
+
+
+	public MetadataPdfExtractor getMetadataPdfExtractor()
+	{
+		return fieldMetadataPdfExtractor;
+	}
+
+	public void setMetadataPdfExtractor(MetadataPdfExtractor inMetadataPdfExtractor)
+	{
+		fieldMetadataPdfExtractor = inMetadataPdfExtractor;
+	}
+
+	public boolean extractData(MediaArchive inArchive, ContentItem inputFile, Asset inAsset)
+	{
+		String type = PathUtilities.extractPageType(inputFile.getPath());
+		if (type == null || "data".equals(type.toLowerCase()))
+		{
+			type = inAsset.getFileFormat();
+		}
+
+		if (type != null)
+		{
+			type = type.toLowerCase();
+		}
+		if( type == null )
+		{
+			return false;
+		}
+		if ( type.equals("pdf"))
+		{
+			return false; //PDF's are already being extracted
+		}
+		
+		if( !FORMATS.contains(type) )
+		{
+			return false;
+		}
+		
+		ContentItem custom = inArchive.getContent( "/WEB-INF/data/" + inArchive.getCatalogId() + "/generated/" + inAsset.getSourcePath() + "/document.pdf");
+		if( !custom.exists() )
+		{
+	        ConversionManager c = inArchive.getTranscodeTools().getManagerByRenderType("document");
+			ConvertInstructions instructions = c.createInstructions();
+			
+			instructions.setAssetSourcePath(inAsset.getSourcePath());
+			instructions.setAsset(inAsset);
+			instructions.setOutputExtension("pdf");
+			instructions.setInputFile(inputFile);
+			instructions.setOutputFile(custom);
+			c.transcode(instructions);
+		}
+	 	//c.(instructions);
+
+		//ooffice can create a PDF then we can pull the size and text from it
+		
+		//String tmppath = getMediaCreator().populateOutputPath(inArchive, inst);
+		
+		//now use the PDF extractor
+		getMetadataPdfExtractor().extractData(inArchive, custom, inAsset);
+		
+//		//now get the page info out of the PDF?
+//		Asset tmp = inArchive.createAsset("tmp/" + inAsset.getSourcePath());
+//		getPdfMetadataExtractor().extractData(inArchive, pdf, tmp);
+//		
+//		inAsset.setProperty(type, tmppath)
+		
+		return true;
+	}
+}
