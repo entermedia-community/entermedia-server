b24868101bef7fd9673158ee151c6e2967984a20
Ian <ian@netevolved.com>
2016-10-06 11:09:31 -0400

    Fixed bug

--- src/org/entermediadb/elasticsearch/searchers/ElasticUserSearcher.java
+++ src/org/entermediadb/elasticsearch/searchers/ElasticUserSearcher.java
@@ -111,7 +111,7 @@ public class ElasticUserSearcher extends BaseElasticSearcher implements UserSear
 	public User getUserByEmail(String inEmail)
 	{
 		User target = null;
-		Data record = (Data)searchByField("email", inEmail);
+		Data record = (Data)query().exact("email", inEmail).searchOne();
 		if(record != null){
 			target = (User) loadData(record);
 		}
