3bf2e32981b76a510e1f070ee83d4818a3dadf43
Ian <ian@netevolved.com>
2016-11-17 09:59:17 -0500

    Pre-delete script

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/assets/delete.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/assets/delete.groovy
@@ -1,8 +1,4 @@
-import org.entermediadb.asset.Asset
 import org.entermediadb.asset.MediaArchive
-import org.openedit.Data
-import org.openedit.data.Searcher
-import org.openedit.event.WebEvent
 
 public init(){
 	log.info("Starting Pre-Deletion Event");
