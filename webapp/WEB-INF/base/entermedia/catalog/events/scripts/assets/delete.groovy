3374715c632e9e13ad9ae298879dd066132584b0
Ian <ian@netevolved.com>
2016-11-21 14:19:21 -0500

    Pre delete script (will use for vizone)

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/assets/delete.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/assets/delete.groovy
@@ -1,9 +1,26 @@
 import org.entermediadb.asset.MediaArchive
+import org.entermediadb.asset.publish.publishers.vizonepublisher;
+import org.openedit.data.Searcher
 
 public init(){
 	log.info("Starting Pre-Deletion Event");
-	MediaArchive mediaArchive = (MediaArchive)context.getPageValue("mediaarchive");
-	
+//	MediaArchive mediaArchive = (MediaArchive)context.getPageValue("mediaarchive");
+//	Searcher targetsearcher = mediaArchive.getAssetSearcher();
+//	String assetids = context.getRequestParameter("assetids");
+//	Collection hits = mediaarchive.getAssetSearcher().query().orgroup("id",assetids).search();
+//	
+//	vizonepublisher vizone = mediaArchive.getModuleManager().getBean(mediaArchive.getCatalogid(), "VizOnepublisher");
+//	
+//	Data publishdestination = 
+//	String servername = inDestination.get("server");
+//	String username = inDestination.get("username");
+//	String url = inDestination.get("url");
+//	
+//	
+//	
+//	//EMDEV:3nterMed1a
+//	
+//	
 }
 
 init();
