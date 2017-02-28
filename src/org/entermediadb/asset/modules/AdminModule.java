949a35e7800688d866cf0b49dee83d527fdbd045
Christopher Burkey <cburkey@openedit.org>
2017-02-28 09:39:18 -0500

    Clean login the user

--- src/org/entermediadb/asset/modules/AdminModule.java
+++ src/org/entermediadb/asset/modules/AdminModule.java
@@ -438,8 +438,8 @@ public class AdminModule extends BaseModule
 				String md5 = getCookieEncryption().getPasswordMd5(user.getPassword());
 				String value = user.getUserName() + "md542" + md5;
 				inReq.putPageValue("entermediakey", value);
-				String catalogid = user.get("catalogid");
-				inReq.putSessionValue(catalogid + "user", user);
+				inReq.putSessionValue(aReq.getCatalogId() + "user", user);
+				inReq.putPageValue("user", user);
 			}
 		}
 		else
