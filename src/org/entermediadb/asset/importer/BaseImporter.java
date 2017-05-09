a792d794f15b7090f8cc47216643a1e04da10071
netevolved <ian@netevolved.com>
2017-05-09 11:25:14 -0400

    Updated to avoid mapping errors

--- src/org/entermediadb/asset/importer/BaseImporter.java
+++ src/org/entermediadb/asset/importer/BaseImporter.java
@@ -3,6 +3,7 @@ package org.entermediadb.asset.importer;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -19,6 +20,7 @@ import org.openedit.data.PropertyDetails;
 import org.openedit.data.Searcher;
 import org.openedit.modules.translations.LanguageMap;
 import org.openedit.page.Page;
+import org.openedit.util.DateStorageUtil;
 import org.openedit.util.EmStringUtils;
 import org.openedit.util.FileUtils;
 import org.openedit.util.PathUtilities;
@@ -266,14 +268,8 @@ public class BaseImporter extends EnterMediaObject
 			
 			if (detail == null && !header.contains("."))
 			{
-				detail = new PropertyDetail();
-				detail.setId(id);
-				detail.setName(header);
-				detail.setEditable(true);
-				detail.setIndex(true);
-				detail.setStored(true);
-				detail.setCatalogId(getSearcher().getCatalogId());
-				details.addDetail(detail);
+				detail = getSearcher().getPropertyDetailsArchive().createDetail(id, id);
+
 				
 				getSearcher().getPropertyDetailsArchive().savePropertyDetail(detail, getSearcher().getSearchType(), context.getUser());
 			}
@@ -326,16 +322,10 @@ public class BaseImporter extends EnterMediaObject
 			else if (val != null && val.length() > 0)
 			{
 				
-				PropertyDetail detail = details.getDetail(headerid);
-				if(detail == null){
-					detail = details.getDetail(header);
-				}
 				
-				if(detail != null){
-					
+				Object value = getSearcher().createValue(headerid, val);
+				inData.setValue(headerid, value);
 				
-				inData.setProperty(detail.getId(), val);
-				}
 			}
 		}
 	}
