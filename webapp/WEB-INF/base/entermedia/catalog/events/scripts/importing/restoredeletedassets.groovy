df8069b57c7d3bdfadb08b4e3d0a8d0fc1292e6a
Ian <ian@netevolved.com>
2017-05-30 15:16:15 -0400

    Updates to make restore deleted smarter when dealing with new assets with old names

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/restoredeletedassets.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/importing/restoredeletedassets.groovy
@@ -1,12 +1,18 @@
 package importing
 
+import org.apache.commons.codec.digest.DigestUtils
 import org.entermediadb.asset.Asset
 import org.entermediadb.asset.MediaArchive
+import org.entermediadb.asset.scanner.PresetCreator;
 import org.entermediadb.asset.search.AssetSearcher
 import org.openedit.Data
+import org.openedit.OpenEditException
+import org.openedit.data.Searcher;
 import org.openedit.hittracker.HitTracker
 import org.openedit.hittracker.SearchQuery
+import org.openedit.page.Page
 import org.openedit.page.manage.PageManager
+import org.openedit.util.FileUtils
 
 public void init()
 {
@@ -60,6 +66,32 @@ public void init()
            {
 			   asset.setProperty("editstatus", "1");
 			   tosave.add(asset);
+			   String currentmd5 = asset.get("md5hex");
+			   if(currentmd5 != null){
+				   String md5;
+				   Page content = pageManager.getPage(pathToOriginal)
+				   InputStream inputStream = content.getInputStream();
+   
+				   try
+				   {
+					   md5 = DigestUtils.md5Hex(inputStream);
+				   }
+				   catch (Exception e)
+				   {
+					   throw new OpenEditException(e);
+				   }
+				   finally
+				   {
+					   FileUtils.safeClose(inputStream);
+				   }
+				   if(!md5.equals(currentmd5)){
+					   //This is a different file!
+					   archive.removeGeneratedImages(asset, true);
+					 	archive.getAssetSearcher().delete(asset, null);
+						 archive.getAssetImporter().createAssetFromPage(archive, null, content);
+					   
+				   }
+			   }
            }
 		}
 		else
