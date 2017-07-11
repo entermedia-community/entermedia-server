ab04560bc67a3375074e57cdded69dd87b91bd31
netevolved <ian@netevolved.com>
2017-07-11 15:22:37 -0400

    sfad

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/vizone/syncassets.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/vizone/syncassets.groovy
@@ -2,11 +2,6 @@ package vizone
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
-import org.apache.http.client.HttpClient
-import org.apache.http.client.config.CookieSpecs
-import org.apache.http.client.config.RequestConfig
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
 import org.entermediadb.asset.Asset
 import org.entermediadb.asset.Category
 import org.entermediadb.asset.ChunkySourcePathCreator
@@ -14,18 +9,18 @@ import org.entermediadb.asset.MediaArchive
 import org.entermediadb.asset.SourcePathCreator
 import org.entermediadb.asset.scanner.MetaDataReader
 import org.entermediadb.asset.search.AssetSearcher
-import org.entermediadb.email.ElasticPostMail
 import org.entermediadb.modules.update.Downloader
 import org.openedit.WebPageRequest
+import org.openedit.hittracker.HitTracker
 import org.openedit.page.Page
 
 public class VizOne{
-	
-	private static final Log log = LogFactory.getLog(ElasticPostMail.class);
-	
+
+	private static final Log log = LogFactory.getLog(VizOne.class);
+
 	def authString = "EMDEV:3nterMed1a".getBytes().encodeBase64().toString();
 	protected ThreadLocal perThreadCache = new ThreadLocal();
-	public void testLoadAsset(WebPageRequest inReq){
+	public void downloadNew(WebPageRequest inReq){
 		//def addr       = "http://vizmtlvamf.media.in.cbcsrc.ca/thirdparty/asset/item?id=50"
 		def addr       = "http://vizmtlvamf.media.in.cbcsrc.ca/thirdparty/asset/item?start=1&num=10000"
 		def conn = addr.toURL().openConnection()
@@ -48,58 +43,60 @@ public class VizOne{
 					if("deleted".equals(it.mediastatus.text())){
 						return;
 					}
+
+
 					String vizid =it.ardomeIdentity;
-					
+
 					Asset asset = assetsearcher.searchByField("vizid", vizid);
-					
+
 					def url = it.'**'.find { link-> link.@rel == 'describedby'};
-					
+
 					String href= url.@href;
-				
-					
-					
+
+
+
 					def conn2 = href.toURL().openConnection()
 					conn2.setRequestProperty( "Authorization", "Basic ${authString}" )
 					conn2.setRequestProperty("Accept", "application/vnd.vizrt.payload+xml");
-					
+
 					String mdata = conn2.content.text;
 					def extradata = new XmlSlurper().parseText(mdata  )
-					
-					
-					
-					
-					
+
+
+
+
+
 					if(asset == null){
-						
-						
+
+
 						asset = assetsearcher.createNewData();
 
-						
-						
-						
+
+
+
 						asset.setValue("vizid", vizid);
 						String itContent = it.content.@src;
 						asset.setValue("fetchurl", itContent);
-					
-						
-					extradata.field.each{
-						String name = it.@name
-						def val = it.'value';
-						
-						String value = it.'value'.text();
-						if(name.contains("importFileName")){
-							asset.setName(value);
+
+
+						extradata.field.each{
+							String name = it.@name
+							def val = it.'value';
+
+							String value = it.'value'.text();
+							if(name.contains("importFileName")){
+								asset.setName(value);
+							}
+							else if(name.contains("retentionPolicy")){
+								asset.setValue("vizoneretention", value);
+							}
 						}
-						else if(name.contains("retentionPolicy")){
-							asset.setValue("vizoneretention", value);
-						}
-					}
-					
-						
-						
+
+
+
 						asset.setValue("importstatus", "needsdownload");
 						SourcePathCreator creator = new ChunkySourcePathCreator();
-						
+
 						String sourcepath = creator.createSourcePath(asset, vizid);
 						asset.setSourcePath("vizone/${sourcepath}");
 						assets.add(asset);
@@ -116,33 +113,33 @@ public class VizOne{
 
 						asset.setValue("importstatus","imported");
 						asset.setValue("fromviz",true);
-						
+
 						def tasksearcher = archive.getSearcher("conversiontask");
 
 						archive.fireMediaEvent( "importing","assetsimported", null, asset); //this will save the asset as imported
 					}
-					
-					
+
+
 					String itTitle = it.title.text();
 					asset.setValue("assettitle", itTitle);
 					asset.setName(it.derivativefilename.text());
 
-					
-					
-//					<field name="vpm.importFileName">
-//					<value>photo 3x4.JPG</value>
-//				  </field>
-//				  <field name="asset.title">
-//					<value>photo 3x4.JPG</value>
-//				  </field>
-//				  <field name="asset.retentionPolicy">
-//					<value>default</value>
-//				  </field>
-					
+
+
+					//					<field name="vpm.importFileName">
+					//					<value>photo 3x4.JPG</value>
+					//				  </field>
+					//				  <field name="asset.title">
+					//					<value>photo 3x4.JPG</value>
+					//				  </field>
+					//				  <field name="asset.retentionPolicy">
+					//					<value>default</value>
+					//				  </field>
+
 					extradata.field.each{
 						String name = it.@name
 						def val = it.'value';
-						
+
 						String value = it.'value'.text();
 						if(name.contains("importFileName")){
 							asset.setName(value);
@@ -151,24 +148,24 @@ public class VizOne{
 							asset.setValue("vizoneretention", value);
 						}
 					}
-					
-										
-					
+
+
+
 					Category cat = archive.getCategorySearcher().searchById("AVyh5_tsmQeu4rFCDJ4S");
 					asset.clearCategories();
 					asset.addCategory(cat);
-					
-					
+
+
 					archive.saveAsset(asset,null);
-					
+
 					//Always update metadata  and save asset in case VIZ metadata changed?
-					
-					
-					
-					
-					
-					
-					
+
+
+
+
+
+
+
 
 				} catch(Exception e){
 
@@ -187,41 +184,97 @@ public class VizOne{
 
 
 
+	public void validate(WebPageRequest inReq){
+		//def addr       = "http://vizmtlvamf.media.in.cbcsrc.ca/thirdparty/asset/item?id=50"
 
 
 
-	public HttpClient getClient(String inCatalogId)
-	{
-		RequestConfig globalConfig = RequestConfig.custom()
-				.setCookieSpec(CookieSpecs.DEFAULT)
-				.build();
-		CloseableHttpClient httpClient = HttpClients.custom()
-				.setDefaultRequestConfig(globalConfig)
-				.build();
+		MediaArchive archive = inReq.getPageValue("mediaarchive");
+		AssetSearcher assetsearcher = archive.getAssetSearcher();
+		ArrayList todelete = new ArrayList();
+		HitTracker assets = assetsearcher.fieldSearch("vizid", "*");
+		assets.each{
+			def addr       = "http://vizmtlvamf.media.in.cbcsrc.ca/thirdparty/asset/item/${it.vizid}";
 
-		return httpClient;
+			def conn = addr.toURL().openConnection()
+			conn.setRequestProperty( "Authorization", "Basic ${authString}" )
+			conn.setRequestProperty("Accept", "application/atom+xml;type=entry");
 
+
+			if( conn.responseCode == 200 ) {
+				Asset asset = archive.getAsset(it.id);
+				String content = conn.content.text;
+				log.info(content);
+				def rss = new XmlSlurper().parseText(content  );
+
+				def url = rss.'**'.find { link-> link.@rel == 'describedby'};
+
+				String href= url.@href;
+
+
+
+				try{def conn2 = href.toURL().openConnection()
+					conn2.setRequestProperty( "Authorization", "Basic ${authString}" )
+					conn2.setRequestProperty("Accept", "application/vnd.vizrt.payload+xml");
+
+					String mdata = conn2.content.text;
+					def extradata = new XmlSlurper().parseText(mdata  )
+					extradata.field.each{
+						String name = it.@name
+						def val = it.'value';
+
+						String value = it.'value'.text();
+						if(name.contains("importFileName")){
+							asset.setName(value);
+						}
+						else if(name.contains("retentionPolicy")){
+							asset.setValue("vizoneretention", value);
+						}
+					}
+				} catch(Exception e){
+					//NO metadata available
+				}
+
+
+				assetsearcher.saveData(asset);
+
+
+
+			} else if (conn.responseCode == 404){
+				todelete.add(it);
+
+			}
+
+
+
+			else{
+				todelete.add(it);
+			}
+
+
+		}
+
+		todelete.each {
+			Asset asset = archive.getAsset(it.id);
+			archive.deleteAsset(asset, true);
+
+		}
 
 
 	}
 
-	
-	
-	
+
+
+
 
 }
 
 
 VizOne vz = new VizOne();
-//vz.testModels();
 
-//vz.testMetadata();
 
-//vz.testCreateRecord();
+vz.downloadNew(context);
+vz.validate(context);
 
-vz.testLoadAsset(context);
-//vz.testUpload(context);
-
-//vz.testSearch();
 
 
