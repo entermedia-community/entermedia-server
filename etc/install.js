687fdcbf0ddf81ca871013778f1e935699a21bd2
netevolved <ian@netevolved.com>
2017-11-08 10:17:21 -0500

    Cleanup

--- etc/install.js
+++ etc/install.js
@@ -45,44 +45,45 @@ files.copyFiles( tmp + "/WEB-INF/base/emgallery", root + "/WEB-INF/base/emgaller
 
 
 log.info("4. REPLACE LIBS");
-files.deleteMatch( web + "/lib/em9_entermedia-server*.jar");
-files.deleteMatch( web + "/lib/em9_entermedia-9*.jar");
-files.deleteMatch( web + "/lib/em9dev_entermedia-server*.jar");
-files.deleteMatch( web + "/lib/em9dev_entermedia-9*.jar");
-
-
-
-files.deleteMatch( web + "/lib/dev_entermedia-server*.jar");
-files.deleteMatch( web + "/lib/dev_entermedia-5*.jar");
-files.deleteMatch( web + "/lib/dev_entermedia-8*.jar");
-
-files.deleteMatch( web + "/lib/entermedia-server*.jar");
-files.deleteMatch( web + "/lib/entermedia-5*.jar");
-files.deleteMatch( web + "/lib/entermedia-7*.jar");
-files.deleteMatch( web + "/lib/entermedia-8*.jar");
-
-
-files.deleteMatch( web + "/lib/groovy-*.jar");
-//files.deleteMatch( web + "/lib/aws-*.jar");
-files.deleteMatch( web + "/lib/gson-*.jar");
-files.deleteMatch( web + "/lib/mp4parser*.jar");
-files.deleteMatch( web + "/lib/isoparser*.jar");
-files.deleteMatch( web + "/lib/PDFBox*.jar");
-files.deleteMatch( web + "/lib/pdfbox*.jar");
-files.deleteMatch( web + "/lib/FontBox*.jar");
-files.deleteMatch( web + "/lib/fontbox*.jar");
-files.deleteMatch( web + "/lib/lucene*.jar");
-files.deleteMatch( web + "/lib/spring-*.jar");
-files.deleteMatch( web + "/lib/aopalliance-*.jar");
-files.deleteMatch( web + "/lib/jaxen-*.jar");
-files.deleteMatch( web + "/lib/commons-codec*.jar");
-files.deleteMatch( web + "/lib/guava-*.jar");
-files.deleteMatch( web + "/lib/velocity-tools*.jar");
-files.deleteMatch( web + "/lib/http*.jar");
-files.deleteMatch( web + "/lib/servlet-gzip.jar");
-files.deleteMatch( web + "/lib/json-simple*.jar");
-files.deleteMatch( web + "/lib/org.apache.oltu.*.jar");
-files.deleteMatch( web + "/lib/slf*.jar");
+//files.deleteMatch( web + "/lib/em9_entermedia-server*.jar");
+//files.deleteMatch( web + "/lib/em9_entermedia-9*.jar");
+//files.deleteMatch( web + "/lib/em9dev_entermedia-server*.jar");
+//files.deleteMatch( web + "/lib/em9dev_entermedia-9*.jar");
+//
+//
+//
+//files.deleteMatch( web + "/lib/dev_entermedia-server*.jar");
+//files.deleteMatch( web + "/lib/dev_entermedia-5*.jar");
+//files.deleteMatch( web + "/lib/dev_entermedia-8*.jar");
+//
+//files.deleteMatch( web + "/lib/entermedia-server*.jar");
+//files.deleteMatch( web + "/lib/entermedia-5*.jar");
+//files.deleteMatch( web + "/lib/entermedia-7*.jar");
+//files.deleteMatch( web + "/lib/entermedia-8*.jar");
+//
+//
+//files.deleteMatch( web + "/lib/groovy-*.jar");
+////files.deleteMatch( web + "/lib/aws-*.jar");
+//files.deleteMatch( web + "/lib/gson-*.jar");
+//files.deleteMatch( web + "/lib/mp4parser*.jar");
+//files.deleteMatch( web + "/lib/isoparser*.jar");
+//files.deleteMatch( web + "/lib/PDFBox*.jar");
+//files.deleteMatch( web + "/lib/pdfbox*.jar");
+//files.deleteMatch( web + "/lib/FontBox*.jar");
+//files.deleteMatch( web + "/lib/fontbox*.jar");
+//files.deleteMatch( web + "/lib/lucene*.jar");
+//files.deleteMatch( web + "/lib/spring-*.jar");
+//files.deleteMatch( web + "/lib/aopalliance-*.jar");
+//files.deleteMatch( web + "/lib/jaxen-*.jar");
+//files.deleteMatch( web + "/lib/commons-codec*.jar");
+//files.deleteMatch( web + "/lib/guava-*.jar");
+//files.deleteMatch( web + "/lib/velocity-tools*.jar");
+//files.deleteMatch( web + "/lib/http*.jar");
+//files.deleteMatch( web + "/lib/servlet-gzip.jar");
+//files.deleteMatch( web + "/lib/json-simple*.jar");
+//files.deleteMatch( web + "/lib/org.apache.oltu.*.jar");
+//files.deleteMatch( web + "/lib/slf*.jar");
+files.deleteAll( root + "/WEB-INF/lib");
 
 
 files.copyFileByMatch( tmp + "/WEB-INF/lib/*.jar", web + "/lib/");
