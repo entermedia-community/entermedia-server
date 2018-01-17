c66d624376f77b9ce2aaa7508611c263ceb0b305
Ian <ian@netevolved.com>
2018-01-17 11:29:25 -0500

    Crop larger images with no loss

--- src/org/entermediadb/asset/modules/ConvertStatusModule.java
+++ src/org/entermediadb/asset/modules/ConvertStatusModule.java
@@ -108,11 +108,52 @@ public class ConvertStatusModule extends BaseMediaModule
 		//TODO: should do some scaling math based on the input file it selects and the numbers we got so there is no loss in quality
 		
 		
-		//if("image1024x768.jpg".equals(preset.get("generatedoutputfile"))){
+		if("image1024x768.jpg".equals(preset.get("generatedoutputfile"))){
 			Page s1024 = getPageManager().getPage("/WEB-INF/data/" + archive.getCatalogId()	+ "/generated/" + asset.getPath() + "/image1024x768.jpg");
 			instructions.setInputFile(s1024.getContentItem());//So it doesn't go back to the original when cropping 
-		//}
+		}
+		
+		
+		if(instructions.getMaxScaledSize().getHeight() > 768 || instructions.getMaxScaledSize().getWidth() > 1024) {
+			//input will be the original
+			double originalheight = Double.parseDouble(asset.get("height"));
+			double originalwidth = Double.parseDouble(asset.get("width"));
+			boolean wide = true;
+			instructions.setInputFile(archive.getOriginalDocument(asset).getContentItem());
+			if(originalheight > originalwidth) {
+				wide = false;				
+			}
+			//{cropheight=165, assetid=AWEEgnrnvcTz0GAGVvnK, presetdataid=test, croplast=true, y1=101, x1=269, force=true, cropwidth=220, crop=true, outputextension=jpg, cachefilename=image.jpg}
+			Double cropheight = Double.parseDouble(instructions.get("cropheight"));
+			Double cropwidth = Double.parseDouble(instructions.get("cropwidth"));
+			Double x1 = Double.parseDouble(instructions.get("x1"));
+			Double y1 = Double.parseDouble(instructions.get("y1"));
 			
+			Double scalefactor;
+			if(wide) {
+				scalefactor = 1024/originalwidth;
+			} else {
+				scalefactor = 768/originalheight;
+			}
+
+			cropheight = cropheight/scalefactor;
+			cropwidth = cropwidth/scalefactor;
+			x1 = x1/scalefactor;
+			y1 = y1/scalefactor;
+			
+			instructions.setProperty("cropheight", Integer.toString(cropheight.intValue()));
+			instructions.setProperty("cropwidth", Integer.toString(cropwidth.intValue()));
+			instructions.setProperty("x1", Integer.toString(x1.intValue()));
+			instructions.setProperty("y1", Integer.toString(y1.intValue()));
+			instructions.setOutputFile(outputpage);
+			
+					
+			
+			
+			
+			
+			
+		}
 		
 		
 		
