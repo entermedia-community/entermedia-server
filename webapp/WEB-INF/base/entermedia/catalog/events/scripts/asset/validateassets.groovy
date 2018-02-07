9d938ba37c4f0dc63cddec66ecc6124492624a43
Ian <ian@netevolved.com>
2018-02-07 13:39:26 -0500

    Validate if assets exist and if originals exist based on a "validation" table

---
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/asset/validateassets.groovy
@@ -0,0 +1,46 @@
+package asset
+
+import org.entermediadb.asset.Asset
+import org.entermediadb.asset.MediaArchive
+import org.openedit.Data
+import org.openedit.data.Searcher
+import org.openedit.hittracker.HitTracker
+import org.openedit.page.manage.PageManager
+import org.openedit.repository.ContentItem
+
+
+
+public init()
+{
+	
+	
+	MediaArchive mediaarchive = (MediaArchive)context.getPageValue("mediaarchive");
+	PageManager pageManager = mediaarchive.getPageManager();
+	
+	
+	Searcher searcher = mediaarchive.getSearcher("validation");
+	HitTracker hits = searcher.getAllHits();
+	hits.each{
+		Data data = searcher.loadData(it);
+		String assetid = data.getAt("assetid");
+		Asset asset = mediaarchive.getAsset(assetid);
+		if(asset == null) {
+			data.setValue("record", false);
+			data.setValue("original", false);
+			
+		}
+		if(asset != null) {
+			data.setValue("record", true);
+			ContentItem item = mediaarchive.getOriginalContent(asset);
+			if(item.exists()) {
+				data.setValue("original", true);
+			} else {
+				data.setValue("original", false)
+			}
+		}
+		searcher.saveData(data);
+	}
+	
+}
+
+init();
