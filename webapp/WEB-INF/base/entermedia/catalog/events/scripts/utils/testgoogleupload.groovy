7ab924ca81431e64301505d3298489920fbca650
netevolved <ian@netevolved.com>
2018-03-15 13:24:08 -0400

    test

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/utils/testgoogleupload.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/utils/testgoogleupload.groovy
@@ -3,6 +3,7 @@ package utils
 import org.entermediadb.asset.Asset
 import org.entermediadb.asset.MediaArchive
 import org.entermediadb.google.GoogleManager
+import org.entermediadb.video.CloudTranscodeManager
 import org.openedit.Data
 import org.openedit.repository.ContentItem
 
@@ -10,12 +11,13 @@ public void runit()
 {
 	
 	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
-	Asset asset = mediaArchive.getAsset("AWHn0CCjkfp3AOE-1w72");
+	Asset asset = mediaArchive.getAsset("AWIp-4xQYLXW0p7SkXIt");
 	ContentItem item = mediaArchive.getOriginalContent(asset);
 	GoogleManager manager = mediaArchive.getBean("googleManager");
 	Data auth = mediaArchive.getData("oauthprovider","google");
 	manager.uploadToBucket(auth, "testbench", item, "{\"name\": \"test\"}");
-	
+	CloudTranscodeManager trans = mediaArchive.getBean("cloudTranscodeManager");
+	trans.tr
 }
 
 runit();
