ed0f30149123f883a2b2839d7794ffe9eee8ac7d
cburkey <cburkey@entermediadb.org>
2018-06-20 17:20:08 -0400

    OI goals

--- src/org/entermediadb/tasks/TaskModule.java
+++ src/org/entermediadb/tasks/TaskModule.java
@@ -1,5 +1,9 @@
 package org.entermediadb.tasks;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.entermediadb.asset.Category;
@@ -54,6 +58,36 @@ public class TaskModule extends BaseMediaModule
 		//TODO: Limit it by department
 		HitTracker tracker = searcher.query().exact("collectionid", collection.getId()).search();
 		inReq.putPageValue("goals", tracker);
+		Collection topgoals = new ArrayList();
+		Collection ten = new ArrayList();
+		topgoals.add(ten);
+		for (Iterator iterator = tracker.iterator(); iterator.hasNext();)
+		{
+			Data hit = (Data) iterator.next();
+			ten.add(hit);
+			if( ten.size() == 10)
+			{
+				if( topgoals.size() == 3)
+				{
+					break;
+				}
+				ten = new ArrayList();
+				topgoals.add(ten);
+			}
+		}
+		if( topgoals.size() ==0)
+		{
+			topgoals.add(new ArrayList());
+		}
+		if( topgoals.size() == 1)
+		{
+			topgoals.add(new ArrayList());
+		}
+		if( topgoals.size() == 2)
+		{
+			topgoals.add(new ArrayList());
+		}
+		inReq.putPageValue("topgoals", topgoals);
 		
 	}
 }
