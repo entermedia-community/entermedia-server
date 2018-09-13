a816e104a1e00b769942708ba48d8f4844615ebd
Ian <ian@netevolved.com>
2018-09-13 14:05:44 -0400

    find error

--- src/org/entermediadb/elasticsearch/searchers/ElasticAssetDataConnector.java
+++ src/org/entermediadb/elasticsearch/searchers/ElasticAssetDataConnector.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -29,16 +27,12 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.entermediadb.asset.Asset;
 import org.entermediadb.asset.AssetArchive;
-import org.entermediadb.asset.Category;
 import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.search.AssetSecurityArchive;
 import org.entermediadb.asset.search.DataConnector;
-import org.entermediadb.asset.util.JsonUtil;
 import org.entermediadb.data.DataArchive;
-import org.entermediadb.elasticsearch.ElasticNodeManager;
 import org.entermediadb.elasticsearch.SearchHitData;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.openedit.Data;
 import org.openedit.MultiValued;
 import org.openedit.OpenEditException;
@@ -570,6 +564,15 @@ public class ElasticAssetDataConnector extends ElasticXmlFileSearcher implements
 			for (Iterator iterator = inJsonArray.iterator(); iterator.hasNext();)
 			{
 				String json = (String) iterator.next();
+				try
+				{
+					parser.parse(json);
+				}
+				catch (Exception e)
+				{
+					log.info("could not parse: " + json);
+					throw new OpenEditException("here is the prob!");
+				}
 				IndexRequest req = Requests.indexRequest(getElasticIndexId()).type("asset");
 				req.source(json);
 				//log.info("savinng " + json);
@@ -591,7 +594,7 @@ public class ElasticAssetDataConnector extends ElasticXmlFileSearcher implements
 		}
 		if(errors.size() > 0) 
 		{
-			throw new OpenEditException("Errors from bulk import!" + errors);
+			
 		}
 
 	}
