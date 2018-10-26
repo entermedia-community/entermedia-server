de3eefe718954d30530654e881ccda1b1582e029
cburkey <cburkey@entermediadb.org>
2018-10-26 16:01:46 -0400

    Site stuff

---
+++ src/org/entermediadb/posts/PostModule.java
@@ -0,0 +1,24 @@
+package org.entermediadb.posts;
+
+import org.openedit.WebPageRequest;
+import org.openedit.modules.BaseModule;
+
+public class PostModule extends BaseModule
+{
+	public void loadPost(WebPageRequest inReq)
+	{
+		//Find the collection id
+		String path = inReq.getPath();
+
+		//Look up the domain?
+		
+		//parentcollectionid
+		
+		//Find the URL path
+		
+		//Search the siteposts table
+		
+		//Load up a $sitehome and $postdata 
+		
+	}
+}
