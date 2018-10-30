742f8ea36d5d6add68e94f54c43adf4f5d819355
Ian <ian@netevolved.com>
2018-10-29 22:01:00 -0400

    All tables

--- src/org/entermediadb/elasticsearch/searchers/BaseElasticSearcher.java
+++ src/org/entermediadb/elasticsearch/searchers/BaseElasticSearcher.java
@@ -1785,15 +1785,15 @@ public class BaseElasticSearcher extends BaseSearcher
 	protected void updateMasterClusterId(PropertyDetails details, Data inData, XContentBuilder content) throws IOException
 	{
 
-		PropertyDetail hasmaster = details.getDetail("mastereditclusterid");
-		if (hasmaster != null)
-		{
-			if (inData.getValue("mastereditclusterid") == null)
-			{
+//		PropertyDetail hasmaster = details.getDetail("mastereditclusterid");
+//		if (hasmaster != null)
+//		{
+//			if (inData.getValue("mastereditclusterid") == null)
+//			{
 				//Add nodeidmaster = dsfsd, also keep track of record edited timestamps
 				content.field("mastereditclusterid", getElasticNodeManager().getLocalClusterId());
-			}
-		}
+//			}
+//		}
 	}
 
 	public void deleteAll(Collection inBuffer, User inUser)
