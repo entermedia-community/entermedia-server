06b6efbce37e255eba35021191129b6d39331df5
Ian <ian@netevolved.com>
2018-11-06 15:01:02 -0500

    Fixed

--- src/org/entermediadb/posts/PostModule.java
+++ src/org/entermediadb/posts/PostModule.java
@@ -8,21 +8,31 @@ public class PostModule extends BaseMediaModule
 {
 	public void loadPost(WebPageRequest inReq)
 	{
-		String path  = inReq.getPath();
-		
-		String sitehome = (String)inReq.getPageValue("sitehome");
-		//String apphome = (String)inReq.getPageValue("apphome");
-		
-		String sourceppath = path.substring(sitehome.length()+1, path.length());
+		String path = inReq.getPath();
+
+		String sitehome = (String) inReq.getPageValue("sitehome");
+		String apphome = (String)inReq.getPageValue("apphome");
+
+		String sourceppath = path.substring(sitehome.length() + 1, path.length());
 		//getSearcherManager().getCacheManager()
 		String catalogid = inReq.findValue("catalogid");
 		//TODO: Cache this
 		//TODO: Fallback? Use the actual page
-		Searcher searcher = getSearcherManager().getSearcher(catalogid,"postdata");
-		PostData post = (PostData)searcher.query().exact("sourcepath", sourceppath).searchOne();
-		if( post == null)
+		Searcher searcher = getSearcherManager().getSearcher(catalogid, "postdata");
+		PostData post = (PostData) searcher.query().exact("sourcepath", sourceppath).searchOne();
+		if (post == null)
 		{
-			post = (PostData)searcher.createNewData();
+			if (path.endsWith("index.html"))
+			{
+				path = path.substring(0, path.length() - 10);
+			}
+		}
+		
+		post = (PostData) searcher.query().exact("sourcepath", path).searchOne();
+
+		if (post == null)
+		{
+			post = (PostData) searcher.createNewData();
 			//post.setValue("siteid", siteid);
 			post.setValue("sourcepath", sourceppath);
 			post.setValue("maincontent", "Hello World");
