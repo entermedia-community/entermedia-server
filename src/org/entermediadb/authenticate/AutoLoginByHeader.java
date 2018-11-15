288c194ba25cc6a2e57caeb9481e1a101471661a
cburkey <cburkey@entermediadb.org>
2018-11-15 09:25:16 -0500

    Added header

--- src/org/entermediadb/authenticate/AutoLoginByHeader.java
+++ src/org/entermediadb/authenticate/AutoLoginByHeader.java
@@ -15,11 +15,19 @@ public class AutoLoginByHeader extends BaseAutoLogin implements AutoLoginProvide
 
 	protected AutoLoginResult autoLoginFromRequest(WebPageRequest inRequest)
 	{
-		String username = inRequest.getRequest().getRemoteUser();
+		String header = inRequest.getContentProperty("autologinheader");
+
+		if( header == null)
+		{
+			return null;
+		}
+		
+		String username = inRequest.getRequest().getHeader(header);
 		if (username == null)
 		{
 			return null;
 		}
+		
 		UserManager userManager = getUserManager(inRequest);
 		User user = userManager.getUser(username);
 		String catalogid = inRequest.findValue("catalogid");
