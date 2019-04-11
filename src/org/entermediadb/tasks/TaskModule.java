c2095e8fcd0ad93db2de32436f480e3d3ef2d6d9
cburkey <cburkey@entermediadb.org>
2019-04-11 10:59:23 -0400

    Fix sorting and auto priority

--- src/org/entermediadb/tasks/TaskModule.java
+++ src/org/entermediadb/tasks/TaskModule.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -192,9 +193,14 @@ public class TaskModule extends BaseMediaModule
 				{
 					GregorianCalendar rg = new GregorianCalendar();
 					rg.setTime(rd);
-					if( (rg.get(Calendar.DAY_OF_WEEK) - 2) == i)
+					int dow = rg.get(Calendar.DAY_OF_WEEK) - 2;
+					if( dow == i)
 					{
 						todaysgoals.add(goal);
+					}
+					else if( dow < i )
+					{
+						goal.setValue("projectstatus", "critical");
 					}
 				}
 			}
@@ -202,7 +208,44 @@ public class TaskModule extends BaseMediaModule
 		for (Iterator iterator = week.iterator(); iterator.hasNext();)
 		{
 			List values = (List ) iterator.next();
-			Collections.sort(values);
+			Collections.sort(values, new Comparator<ProjectGoal>()
+			{
+				@Override
+				public int compare(ProjectGoal pg1, ProjectGoal pg2)
+				{
+						String status1 = pg1.get("projectstatus");
+						String status2 = pg2.get("projectstatus");
+						if( status1 == null) status1 = "open";
+						if( status2 == null) status2 = "open";
+						if( status1.equals(status2))
+						{
+							Date date1 = (Date)pg1.getDate("creationdate");
+							Date date2 = (Date)pg2.getDate("creationdate");
+							if( date1 != null && date2 != null)
+							{
+								return date2.compareTo(date1);
+							}
+						}
+						else if( status1.equals("critical"))
+						{
+							return 1;
+						}
+						else if( status2.equals("critical"))
+						{
+							return -1;
+						}
+						else if( status1.equals("open"))
+						{
+							return -1;
+						}
+						else if( status2.equals("open"))
+						{
+							return 0;
+						}
+						
+						return 0;
+					}
+			});
 			Collections.reverse(values);
 		}
 		return week;
