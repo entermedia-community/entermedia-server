dd8acf16685105051910aa4aef57680f5b06bd16
Thomas N <t.nadalie33@gmail.com>
2019-10-30 12:06:32 -0600

    Prep autocomplete without counts

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/asset/validateautocomplete.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/asset/validateautocomplete.groovy
@@ -3,6 +3,7 @@ package asset;
 import org.entermediadb.asset.MediaArchive
 import org.entermediadb.asset.search.AssetSearcher
 import org.openedit.Data
+import org.openedit.MultiValued
 import org.openedit.data.Searcher
 import org.openedit.hittracker.HitTracker
 import org.openedit.util.DateStorageUtil
@@ -11,38 +12,87 @@ public void init(){
 	
 	MediaArchive mediaarchive = (MediaArchive)context.getPageValue("mediaarchive");
 	
-	
 	Searcher autocompletesearcher = mediaarchive.getSearcher("assetAutoComplete");
 	
 	AssetSearcher searcher = mediaarchive.getAssetSearcher();
 	
 	HitTracker hits = autocompletesearcher.getAllHits();
+	List toDelete = new ArrayList();
+	
 	hits.each{
 		Data word = autocompletesearcher.loadData(it);
 		
-		def search = word.get("synonyms");
-		if(search){
-		HitTracker assets = searcher.query().freeform("description", search).search();
 		
-		if(assets.size() == 0){
-			autocompletesearcher.delete(word, null)
-		} else{
-			word.setProperty("hitcount", String.valueOf( assets.size() ) );
-			word.setProperty("timestamp", DateStorageUtil.getStorageUtil().formatForStorage(new Date()) );
-			autocompletesearcher.saveData(word);
+		if (word.getValue("autopopulated") == true)
+		{
+			toDelete.add(word);
+		}		
+		else {
+			def search = word.get("synonyms");
+			if(search){
+				HitTracker assets = searcher.query().freeform("description", search).search();
+				
+				if(assets.size() == 0){
+					autocompletesearcher.delete(word, null)
+				} else{
+					word.setProperty("hitcount", String.valueOf( assets.size() ) );
+					word.setProperty("timestamp", DateStorageUtil.getStorageUtil().formatForStorage(new Date()) );
+					autocompletesearcher.saveData(word);
+				}
+			}
 		}
-		}
-		
-		
-		
 	}
+	autocompletesearcher.deleteAll(toDelete, user);
 	
+	Set<String> assetTags = new HashSet();
+	HitTracker assets = searcher.getAllHits();
+	List toSave= new ArrayList();
+	assets.each 
+	{ 
+		MultiValued asset = it;
+		String text = asset.get("name");
+		Collection tags = asset.getValues("keywords");
+		
+		if (tags != null ) {
+			assetTags.addAll(tags);
+		}
+		
+		Data word = autocompletesearcher.createNewData();
+		word.setId(text.toLowerCase());  //THIS makes them only save one copy
+		word.setValue("synonyms", text);
+		word.setValue("timestamp", DateStorageUtil.getStorageUtil().formatForStorage(new Date()) );
+		word.setValue("synonymsenc",text); //This can be removed if you change the search term to match		
+		word.setValue("autopopulated",true);
+		toSave.add(word);
+		if (toSave.size() > 1000)
+		{
+			autocompletesearcher.saveAllData(toSave, user);
+			toSave.clear();
+		}
+	}
+	autocompletesearcher.saveAllData(toSave, user);
+	toSave.clear();
 	
-	
+	log.info("that's my loop: " + assetTags.size());
+	assetTags.each
+	{
+		String text = it;
+
+		Data word = autocompletesearcher.createNewData();
+		word.setId(text.toLowerCase());  //THIS makes them only save one copy
+		word.setValue("synonyms", text);
+		word.setValue("timestamp", DateStorageUtil.getStorageUtil().formatForStorage(new Date()) );
+		word.setValue("synonymsenc",text); //This can be removed if you change the search term to match
+		word.setValue("autopopulated",true);
+		toSave.add(word);
+		if (toSave.size() > 1000)
+		{
+			autocompletesearcher.saveAllData(toSave, user);
+			toSave.clear();
+		}
+
+	}
+	autocompletesearcher.saveAllData(toSave, user);
 }
-
-
-
-
 
 init();
