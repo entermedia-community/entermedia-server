54cf0e95349adafb59d9f8b3381cc6359e177fc3
Cristobal Mejia <cristobal@openedit.org>
2020-06-08 15:09:58 -0500

    Google Vision, JSON library

--- webapp/WEB-INF/base/entermedia/catalog/events/scripts/asset/applygooglevision.groovy
+++ webapp/WEB-INF/base/entermedia/catalog/events/scripts/asset/applygooglevision.groovy
@@ -12,8 +12,8 @@ import org.openedit.Data
 import org.openedit.data.QueryBuilder
 import org.openedit.hittracker.HitTracker
 
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public void runit()
 {
@@ -30,19 +30,11 @@ public void runit()
 		return null;
 	}
 	
-	//Search collections marked for processing
+	//Search assets not tagged and importstatus complete
 	QueryBuilder query = mediaArchive.getAssetSearcher().query().exact("googletagged", "false").exact("importstatus", "complete");
 
+	//System Wide Enabled 
 	String systemwidetagging = mediaArchive.getCatalogSettingValue("systemwidetagging");
-	/*Google vision date filter*/
-	String googlevisionstartdate = mediaArchive.getCatalogSettingValue("google_api_start_date");
-	if (googlevisionstartdate == null)
-	{
-		googlevisionstartdate = "01/01/2000";
-	}
-	DateFormat format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
-	Date date = format.parse(googlevisionstartdate);
-	
 	if( !Boolean.parseBoolean(systemwidetagging))
 	{
 		Collection cats = new HashSet();
@@ -57,70 +49,83 @@ public void runit()
 		}
 		if(cats.isEmpty())
 		{
-			log.info("No collections are marked as automatic");
+			log.info("No collections are marked as automatic Google Tagging");
 			return;
 		}
-		query.orgroup("category", cats).after("assetaddeddate",date);
-	}	
-	HitTracker hits = query.search();
-	
-	log.info(hits.size()+" assets to be tagged by Google. System wide:"  + query);
-	Integer assetcount = 0;
-	
-	List tosave = new ArrayList();
-	hits.each
-	{
-		Data hit = it;
-		Asset asset = mediaArchive.getAsset(it.id);
-		
-		JsonObject object = manager.processImage(connection,asset);
-		if(object == null) {
-			return;
-		}
-		
-		JsonObject responselist = object.getAsJsonArray("responses").get(0);
-		JsonArray labels = responselist.getAsJsonArray("labelAnnotations");
-		labels.each{
-			String tag = it.description.getAsString();
-			asset.addValue("googlekeywords", tag);
-			asset.setValue("googletagged", "true");
-		}
-		JsonArray others = responselist.getAsJsonArray("localizedObjectAnnotations");
-		others.each { 
-			String tag = it.name.getAsString();
-			asset.addValue("googlekeywords", tag);
-			asset.setValue("googletagged", "true");
-			
-		}
-		/*
-		others = responselist.getAsJsonArray("landmarkAnnotations");
-		"locations": [
-			{
-			  "latLng": {
-				"latitude": 55.752912,
-				"longitude": 37.622315883636475
-			  }
-			}
-		  ]
-		others.each {
-			String tag = it.name.getAsString();
-			asset.addValue("googlekeywords", tag);
-			asset.setValue("googletagged", "true");
-		}
-		*/
-		tosave.add(asset);
-		
-		assetcount++;
-		if( tosave.size() == 200)
-		{
-			mediaArchive.saveAssets(tosave);
-			tosave.clear();
-			log.info(assetcount+" assets tagged by Google.");
-		}
+		query.orgroup("category", cats);
 	}
-	mediaArchive.saveAssets(tosave);
-	log.info(assetcount+" assets tagged by Google.")
 	
+	//Google vision date filter
+	String googlevisionstartdate = mediaArchive.getCatalogSettingValue("google_api_start_date");
+	if (googlevisionstartdate == null)
+	{
+		googlevisionstartdate = "01/01/2020";
+	}
+	DateFormat format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+	Date date = format.parse(googlevisionstartdate);
+	query.after("assetaddeddate",date);
+	
+	HitTracker hits = query.search();
+	if (hits.size() > 1) {
+		log.info(hits.size()+" assets to be tagged by Google. System wide:"  + query);
+		Integer assetcount = 0;
+		
+		List tosave = new ArrayList();
+		hits.each
+		{
+			Data hit = it;
+			Asset asset = mediaArchive.getAsset(it.id);
+			
+			//JsonElement jelement = new JsonParser(manager.processImage(connection,asset));
+			JSONObject object = manager.processImage(connection,asset);
+			//JsonArray object = (JsonArray) manager.processImage(connection,asset);
+			if(object == null) {
+				return;
+			}
+			//log.info(object);
+			JSONObject responselist = (JSONObject) object.get("responses");
+			JSONArray labels = (JSONArray) responselist.get("labelAnnotations");
+			labels.each{
+				String tag = it.description.toString();
+				asset.addValue("googlekeywords", tag);
+				asset.setValue("googletagged", "true");
+			}
+			JSONArray others = (JSONArray) responselist.get("localizedObjectAnnotations");
+			others.each { 
+				String tag = it.name.toString();
+				asset.addValue("googlekeywords", tag);
+				asset.setValue("googletagged", "true");
+				
+			}
+			/*
+			others = responselist.getAsJsonArray("landmarkAnnotations");
+			"locations": [
+				{
+				  "latLng": {
+					"latitude": 55.752912,
+					"longitude": 37.622315883636475
+				  }
+				}
+			  ]
+			others.each {
+				String tag = it.name.getAsString();
+				asset.addValue("googlekeywords", tag);
+				asset.setValue("googletagged", "true");
+			}
+			*/
+			tosave.add(asset);
+			
+			assetcount++;
+			if( tosave.size() == 200)
+			{
+				mediaArchive.saveAssets(tosave);
+				tosave.clear();
+				log.info(assetcount+" assets tagged by Google.");
+			}
+		}
+		mediaArchive.saveAssets(tosave);
+		log.info(assetcount+" assets tagged by Google.")
+	}
 }
 
 runit();
