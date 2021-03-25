724e19ae4619d6efc6ad16810da1ea494d5d3990
Christopher Burkey <cburkey@entermediadb.org>
2021-03-25 17:13:20 -0400

    Added combiner

--- src/org/entermediadb/asset/generators/TempFileGenerator.java
+++ src/org/entermediadb/asset/generators/TempFileGenerator.java
@@ -6,6 +6,7 @@ package org.entermediadb.asset.generators;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,9 +16,24 @@ import org.openedit.WebPageRequest;
 import org.openedit.generators.BaseGenerator;
 import org.openedit.generators.Output;
 import org.openedit.util.FileUtils;
+import org.openedit.util.SimpleDateFormatPerThread;
 
 public abstract class TempFileGenerator   extends BaseGenerator implements Generator
 {
+	protected SimpleDateFormatPerThread fieldLastModFormat;
+	
+	public SimpleDateFormatPerThread getLastModFormat() 
+	{
+		if( fieldLastModFormat  == null)
+		{
+			//Tue, 05 Jan 2010 14:20:51 GMT  -- just english
+			fieldLastModFormat = new SimpleDateFormatPerThread("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+			//log.info( fieldLastModFormat.format(new Date()) );
+		}
+		return fieldLastModFormat;
+	}
+
+	
 	public void generate(WebPageRequest inReq, File inFile, Output inOut) throws OpenEditException
 	{
 		//sometimes we can specify the length of the document
