d0d3feaa4c0b0c274f188737d9cf6cfddc80236e
Cristobal Mejia <cristobal@openedit.org>
2021-06-11 16:23:25 -0500

    TL Action buttons

--- webapp/WEB-INF/base/emfrontend2/components/timeline/timeline.js
+++ webapp/WEB-INF/base/emfrontend2/components/timeline/timeline.js
@@ -36,7 +36,7 @@
 	var tlbg = $("#timelinebg");
 	var tlv = $("#timelineviewer");
 	//tlv.width(tlbg.width()+100);
-	tlv.css("min-width", tlbg.width()+140 + "px");
+	tlv.css("min-width", tlbg.width()+40 + "px");
 	
 	var timelineeditor = $(this);
 	var parentoffset = 0;
