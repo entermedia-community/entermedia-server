0b16c74cdf763c2ec0ba272fe1ba62c0c517b12b
Christopher Burkey <cburkey@entermediadb.org>
2021-06-11 18:11:40 -0600

    HSL speed up

--- src/org/entermediadb/asset/convert/transcoders/FfmpegVideoTranscoder.java
+++ src/org/entermediadb/asset/convert/transcoders/FfmpegVideoTranscoder.java
@@ -66,6 +66,8 @@ public class FfmpegVideoTranscoder extends BaseTranscoder
 		comm.add("empty_output");
 		comm.add("-i");
 		comm.add(inputpage.getAbsolutePath());
+		comm.add("-preset");
+		comm.add("ultrafast");
 		comm.add("-y");
 
 		StringBuffer path = new StringBuffer();
@@ -118,7 +120,7 @@ public class FfmpegVideoTranscoder extends BaseTranscoder
 		
 		 :force_original_aspect_ratio=decrease removed because sometimes it gives odd numbers. which isn't compatible with m3u8
 		*/		
-		String command = "-vf scale=w=-2:h=" + inSize;
+		String command = "-preview ultrafast -vf scale=w=-2:h=" + inSize;
 		//command = command + " -c:a aac -ar 48000 -c:v h264 -profile:v baseline -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -crf 28 -b:a 96k";
 		//removing (-profile:v baseline) to let ffmpeg select one profile
 		command = command + " -c:a aac -ar 48000 -c:v h264 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -crf 28 -b:a 96k";
