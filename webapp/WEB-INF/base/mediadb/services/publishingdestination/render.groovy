edd579521a2368b7729bed3a235722b8f7c376dd
Cristobal <cristobal.mejia@gmail.com>
2024-04-18 17:03:42 -0600

    oembed

---
+++ webapp/WEB-INF/base/mediadb/services/publishingdestination/render.groovy
@@ -0,0 +1,27 @@
+package modules;
+
+import org.openedit.Data
+import org.openedit.data.*
+import org.openedit.page.Page
+import org.openedit.hittracker.HitTracker
+
+
+public void init(){
+
+	String applicationid = context.findPathValue("applicationid");
+	
+	Page preview = pageManager.getPage("/"+ applicationid + "/services/publishingdestination/preview.html");
+	
+	StringWriter output = new StringWriter();
+	
+	preview.generate(context, output);
+	
+	context.putPageValue("output", output.toString()); 
+	
+	log.info(output.toString());
+}
+
+
+init();
+
+
