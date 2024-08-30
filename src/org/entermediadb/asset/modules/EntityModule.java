f3f6f9778424f31471203f8a790025a18c62b2eb
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2024-08-31 04:36:20 +0600

    minor cahnges

--- src/org/entermediadb/asset/modules/EntityModule.java
+++ src/org/entermediadb/asset/modules/EntityModule.java
@@ -813,7 +813,7 @@ public class EntityModule extends BaseMediaModule
 			tmpentity.setValue("entitysourcetype", moduleid);
 			tmpentity.setName(names[i]);
 			
-			String path = archive.getEntityManager().loadUploadSourcepath(module,tmpentity,inReq.getUser());
+			String path = archive.getEntityManager().loadUploadSourcepath(module,tmpentity,inReq.getUser(), false);
 			
 			Data existing = archive.query(moduleid).exact("uploadsourcepath", path).searchOne();
 			
