e79cd031f98922de0d5f7353b76c6861f4374dd4
Ian <ian@oneliveweb.com>
2024-12-02 11:14:12 -0600

    Update Java code to match new paths.

--- src/org/entermediadb/modules/publishing/ContentManager.java
+++ src/org/entermediadb/modules/publishing/ContentManager.java
@@ -456,7 +456,9 @@ public class ContentManager implements CatalogEnabled {
 		Category cat = mediaArchive.getEntityManager().createDefaultFolder(entity, null);
 		// Render DITAS for each question and a map
 		String searchhome = inReq.findPathValue("searchhome");
-		Page ditatemplate = mediaArchive.getPageManager().getPage(	searchhome + "/renderdita/templatedita.dita");
+		//TODO: These files should be in the catalog in my opinion so they can be consistently accessed from mediadb etc
+		
+		Page ditatemplate = mediaArchive.getPageManager().getPage(	searchhome + "/editors/dita/renderdita/templatedita.dita");
 		PropertyDetail detail = mediaArchive.getSearcher(targetmodule).getDetail("name");
 
 		WebPageRequest newcontext = inReq.copy(ditatemplate);
@@ -531,7 +533,7 @@ public class ContentManager implements CatalogEnabled {
 
 		mediaArchive.saveData("targetmodule", tosave);
 
-		Page ditatemplatemap = mediaArchive.getPageManager().getPage( searchhome  +"/renderdita/templateditamap.ditamap");
+		Page ditatemplatemap = mediaArchive.getPageManager().getPage( searchhome  +"/editors/dita/renderdita/templateditamap.ditamap");
 
 		StringWriter output = new StringWriter();
 		newcontext = inReq.copy(ditatemplatemap);
