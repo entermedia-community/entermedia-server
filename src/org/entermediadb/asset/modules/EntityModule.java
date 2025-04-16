c0403a4a43c9124d919b62c39c52fd46979c256e
Christopher Burkey <cburkey@entermediadb.org>
2025-04-16 13:05:20 -0600

    fix mergbe

--- src/org/entermediadb/asset/modules/EntityModule.java
+++ src/org/entermediadb/asset/modules/EntityModule.java
@@ -1378,13 +1378,7 @@ public class EntityModule extends BaseMediaModule
 				}
 			}
 		}
-<<<<<<< HEAD
-		archive.saveData(module.getId(),entity);
-		
 		archive.getPermissionManager().checkEntityCategoryPermission(module, (MultiValued)entity);
-=======
-		archive.getPermissionManager().updateEntityCategoryPermissions(module, (MultiValued)entity);
->>>>>>> refs/remotes/origin/em11
 		
 		archive.saveData(module.getId(),entity);
 		
@@ -1411,15 +1405,8 @@ public class EntityModule extends BaseMediaModule
 		entity.removeValue("editorroles", dataid);			
 		entity.removeValue("editorgroups", dataid);			
 
-<<<<<<< HEAD
-		archive.saveData(module.getId(),entity);
-		
 		archive.getPermissionManager().checkEntityCategoryPermission(module, (MultiValued)entity);
-=======
-		archive.getPermissionManager().updateEntityCategoryPermissions(module, (MultiValued)entity);
-	
 		archive.saveData(module.getId(),entity);
->>>>>>> refs/remotes/origin/em11
 	}
 /*
 	public void removeEntityPermission(WebPageRequest inReq) 
