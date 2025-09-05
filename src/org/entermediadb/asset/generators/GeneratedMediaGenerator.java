8bc4314df1e68af63cb2173d4755483c74d311e5
Cristobal <cristobal.mejia@gmail.com>
2025-09-05 11:45:52 -0600

    replace 550x350 image preset to 550x550, added legacy support.

--- src/org/entermediadb/asset/generators/GeneratedMediaGenerator.java
+++ src/org/entermediadb/asset/generators/GeneratedMediaGenerator.java
@@ -128,7 +128,30 @@ public class GeneratedMediaGenerator extends FileGenerator
 		else
 		{
 			output = getPageManager().getPage("/WEB-INF/data/" + catalogid + "/generated" + endingpath);
+			
+			if (!output.exists() && endingpath.contains("image550x350"))
+			{
+				String fileext = PathUtilities.extractPageType(endingpath);
+				String basepath = PathUtilities.extractDirectoryPath(endingpath);
+				
+				endingpath = basepath + "/image550x550." + fileext;
+				
+				output = getPageManager().getPage("/WEB-INF/data/" + catalogid + "/generated" + endingpath);
+
+			}
+			else if (!output.exists() && endingpath.contains("image550x550"))
+			{
+				String fileext = PathUtilities.extractPageType(endingpath);
+				String basepath = PathUtilities.extractDirectoryPath(endingpath);
+				
+				endingpath = basepath + "/image550x350." + fileext;
+				
+				output = getPageManager().getPage("/WEB-INF/data/" + catalogid + "/generated" + endingpath);
+
+			}
 		}
+		
+		
 
 		if (!existed && !output.exists())
 		{
