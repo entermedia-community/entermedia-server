679655dd9a525cdd38c11abfd2d0dfeb1eb1e7ae
Christopher Burkey <cburkey@entermediadb.org>
2025-09-16 13:33:02 -0600

    Added loop

--- src/org/entermediadb/ai/classify/NamedEntityRecognitionManager.java
+++ src/org/entermediadb/ai/classify/NamedEntityRecognitionManager.java
@@ -1,32 +1,130 @@
 package org.entermediadb.ai.classify;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.entermediadb.ai.llm.LlmConnection;
 import org.entermediadb.ai.llm.LlmResponse;
 import org.entermediadb.asset.Asset;
-import org.entermediadb.scripts.ScriptLogger;
 import org.json.simple.JSONObject;
+import org.openedit.Data;
 import org.openedit.MultiValued;
 import org.openedit.data.PropertyDetail;
-import org.openedit.users.User;
 
-public class NamedEntityRecognitionManager extends ClassifyManager {
+public class NamedEntityRecognitionManager extends ClassifyManager 
+{
+	private static final Log log = LogFactory.getLog(NamedEntityRecognitionManager.class);
 
-	// @Override
-	// protected boolean processOneAsset(MultiValued inConfig, LlmConnection llmvisionconnection, LlmConnection llmsemanticconnection, Map<String, String> models, Asset asset) throws Exception
-	// {
-	// 	Collection<Map> tables = (Collection<Map>) inConfig.getValue("tables");
-	// 	if (tables == null || tables.isEmpty())
-	// 	{
-	// 		return false;
-	// 	}
+	 @Override
+	 protected boolean processOneAsset(MultiValued inConfig, LlmConnection llmvisionconnection, LlmConnection llmsemanticconnection, Map<String, String> models, Asset inData)
+	 {
+		 boolean ok = processOneEntity(inConfig,llmvisionconnection,llmsemanticconnection,models,inData,"asset");
+		 return ok;
+	 }
+	 
+	 @Override
+	 protected boolean processOneEntity(MultiValued inConfig, LlmConnection llmvisionconnection, LlmConnection llmsemanticconnection, Map<String, String> models, MultiValued inData, String inModuleId)
+	 {
+	 	Collection<Map> tables = (Collection<Map>) inConfig.getValue("tables");
+	 	if (tables == null || tables.isEmpty())
+	 	{
+	 		return false;
+	 	}
+	 	///Search for all nounds based on tables
+		Map<String,Map> contextfields = populateFields(inModuleId,inData);
+		if(contextfields.isEmpty())
+		{
+			log.info("No fields to check for names in " + inData.getId() + " " + inData.getName());
+			return false;
+		}
+
+ 		Map params = new HashMap();
+ 		params.put("data", inData);
+ 		params.put("contextfields", contextfields);
+ 		
+		String requestPayload = llmvisionconnection.loadInputFromTemplate("/" +  getMediaArchive().getMediaDbId() + "/ai/default/systemmessage/analyzenamed.html", params); 
+		String functionname = inConfig.get("aifunctionname");
+		LlmResponse results = llmvisionconnection.callClassifyFunction(params, models.get("vision"), functionname, requestPayload, null);
+		if (results != null)
+ 		{
+ 			JSONObject arguments = results.getArguments();
+ 			if (arguments != null) 
+ 			{
+ 				Map metadata =  (Map) arguments.get("metadata");
+ 				if (metadata == null || metadata.isEmpty())
+ 				{
+ 					return false;
+ 				}
+ 				for (Iterator iterator2 = metadata.keySet().iterator(); iterator2.hasNext();)
+ 				{
+ 					String inKey = (String) iterator2.next();
+ 					PropertyDetail detail = getMediaArchive().getSearcher(inModuleId).getDetail(inKey);
+ 					if (detail != null)
+ 					{
+ 						String value = (String)metadata.get(inKey);
+ 						if(detail.isList())
+ 						{
+ 							Data savedrecord = saveIfNeeded(inConfig, detail, value);
+ 							if( savedrecord != null)
+ 							{
+ 								inData.addValue(detail.getId(), savedrecord.getId());
+ 							}
+ 						}
+ 					}
+ 				}
+ 			}
+
+ 		}
+	 	
+	 	//Then See if the field exists on the target table on a data case by case basis and add to it. Create the record if needed
 		
+		return true;
+	 	
+	 }
+
+	protected Data saveIfNeeded(MultiValued inConfig, PropertyDetail inDetail, String inValue)
+	{
+		String[] returned = inValue.split(":");
+		
+		String aisourcetype = returned[0];
+		String label = returned[1];
+
+		Map config = findConfigForTable(inConfig,aisourcetype);
+		if( config == null )
+		{
+			return null;
+		}
+		//person:John Smith
+		String listid = inDetail.getId();
+		Data found = getMediaArchive().query(listid).match("name",label).searchOne();
+		if( found == null && Boolean.parseBoolean( (String)config.get("autocreate") ) )
+		{
+			found = getMediaArchive().getSearcher(listid).createNewData();
+			found.setName(label);
+			getMediaArchive().saveData(listid,found);
+		}
+		return found;
+	}
+
+	private Map findConfigForTable(MultiValued inConfig, String inListid)
+	{
+		Collection<Map> tables = (Collection<Map>) inConfig.getValue("tables");
+		for (Iterator iterator = tables.iterator(); iterator.hasNext();)
+		{
+			Map table = (Map) iterator.next();
+			if( inListid.equals( table.get("sourcetype") ) )
+			{
+				//Save this to DB
+				break;
+			}
+				
+		}
+		return null;
+	}	
 		
 	// 	Collection allaifields = getMediaArchive().getAssetPropertyDetails().findAiCreationProperties();
 	// 	Collection aifields = new ArrayList();
