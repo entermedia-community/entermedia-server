affa40e677816175a7766bd80a07e400cff30b4f
Christopher Burkey <cburkey@entermediadb.org>
2025-09-17 10:38:17 -0600

    Translate refactor

--- src/org/entermediadb/translator/TranslationManager.java
+++ src/org/entermediadb/translator/TranslationManager.java
@@ -1,16 +1,10 @@
 package org.entermediadb.translator;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -18,7 +12,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.entermediadb.asset.Asset;
+import org.entermediadb.ai.informatics.InformaticsProcessor;
 import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.net.HttpSharedConnection;
 import org.entermediadb.scripts.ScriptLogger;
@@ -27,16 +21,13 @@ import org.json.simple.JSONObject;
 import org.openedit.CatalogEnabled;
 import org.openedit.Data;
 import org.openedit.ModuleManager;
+import org.openedit.MultiValued;
 import org.openedit.WebPageRequest;
 import org.openedit.data.PropertyDetail;
-import org.openedit.data.PropertyDetails;
-import org.openedit.data.QueryBuilder;
-import org.openedit.data.Searcher;
 import org.openedit.hittracker.HitTracker;
 import org.openedit.modules.translations.LanguageMap;
-import org.openedit.util.DateStorageUtil;
 
-public class TranslationManager implements CatalogEnabled {
+public class TranslationManager extends InformaticsProcessor implements CatalogEnabled {
 	
 	private static Log log = LogFactory.getLog(TranslationManager.class);
 
@@ -121,6 +112,17 @@ public class TranslationManager implements CatalogEnabled {
 		return results;
 		
 	}
+	
+	public void translateAssets(WebPageRequest context, ScriptLogger inLog)
+	{
+		HitTracker assets = (HitTracker) context.getPageValue("assetsToTranslate");
+	
+		MultiValued config = (MultiValued)getMediaArchive().getCachedData("informatics","autotranslate");
+		
+		Collection<MultiValued> records = new ArrayList(assets);
+		processInformaticsOnAssets(inLog, config, records);
+	}
+	
 	public LanguageMap translateField(String field, LanguageMap languageMap, String sourceLang, Collection<String> targetLangs)
 	{
 		ArrayList<String> fieldNames = new ArrayList();
@@ -184,11 +186,10 @@ public class TranslationManager implements CatalogEnabled {
 			{
 				return null;
 			}
-			
-			
 			return processTranslations(translations, fieldNames, sourceLangMap, targetLangs);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) 
+		{
+			log.error("Problem translating " + fieldValues, e);
 			return null;
 		}
 	}
@@ -272,16 +273,16 @@ public class TranslationManager implements CatalogEnabled {
 		
 		return sourceLangMap;
 	}
-	
-	
-	
-	public void translateAssets(WebPageRequest context, ScriptLogger inLog)
+	@Override
+	public void processInformaticsOnAssets(ScriptLogger inLog, MultiValued inConfig, Collection<MultiValued> inAssets)
 	{
-		MediaArchive archive = getMediaArchive();
-		Searcher searcher = archive.getAssetSearcher();
-		PropertyDetails details = searcher.getPropertyDetails();
-		
-		HitTracker locales = archive.query("locale").exact("translatemetadata", true).search();
+		processInformaticsOnEntities(inLog,inConfig,inAssets);
+	}
+	
+	@Override
+	public void processInformaticsOnEntities(ScriptLogger inLog, MultiValued inConfig, Collection<MultiValued> inRecordsToTranslate)
+	{
+		HitTracker locales = getMediaArchive().query("locale").exact("translatemetadata", true).search();
 		
 		if (locales.size() == 1 && "en".equals(locales.get(0).getId())) 
 	    {
@@ -315,72 +316,38 @@ public class TranslationManager implements CatalogEnabled {
 			}
 		}
 		
-		HitTracker assets = (HitTracker) context.getPageValue("assetsToTranslate");
-		
-		if( assets == null || assets.isEmpty())
-		{
-			QueryBuilder query = searcher.query();
-			query.exact("previewstatus", "2")
-				.exact("taggedbyllm", true)
-				.exact("translatesuccess",false)
-				.exact("translaterror",false);
-	
-			String startdate = getMediaArchive().getCatalogSettingValue("ai_metadata_startdate");
-			if (startdate != null)
-			{
-				Date date = DateStorageUtil.getStorageUtil().parseFromStorage(startdate);
-				query.after("assetaddeddate", date);
-			}
-			
-			assets = query.search();
-			assets.enableBulkOperations();
-		}
-		
-		if(assets.isEmpty())
-		{
-			inLog.info("No asset found for Metadata Translation");
-			return;
-		}
-		
-		log.info("Translating " + assets.size() + "("+ assets.getFriendlyQuery() +") assets to " + targetLangs);
-
-		Collection<String> checkfields = Arrays.asList(
-			"headline",
-			"longcaption",
-			"assettitle",
-			"alternatetext"
-		);
+		log.info("Translating " + inRecordsToTranslate + " with " + targetLangs);
 
 		int count = 1;
-		List tosave = new ArrayList();
-		for (Iterator iterator = assets.iterator(); iterator.hasNext();)
+		for (Iterator iterator = inRecordsToTranslate.iterator(); iterator.hasNext();)
 		{
-			Data data = (Data) iterator.next();
-			Asset asset = (Asset) archive.getAssetSearcher().loadData(data);
-			inLog.info("Translating asset (" + count + "/" + assets.size() + ") Id: " + asset.getId() + ", " + asset.getName());
+			MultiValued data = (MultiValued) iterator.next();
+			String moduleid = data.get("entitysourcetype");
+			inLog.info("Translating (" + count + "/" + inRecordsToTranslate.size() + ") type: " + moduleid + ", " + data.getName());
 			count++;
 
-			try{
-				long startTime = System.currentTimeMillis();
+			long startTime = System.currentTimeMillis();
 
-				Map<String, LanguageMap> fieldsmap = new HashMap();
+			Map<String, LanguageMap> fieldsmap = new HashMap();
 
-				for (Iterator iterator2 = checkfields.iterator(); iterator2.hasNext();)
+			Map<String, PropertyDetail> detailsfields = loadActiveDetails(moduleid);
+			
+			for (Iterator iterator2 = detailsfields.keySet().iterator(); iterator2.hasNext();)
+			{
+				String inKey = (String) iterator2.next();
+				PropertyDetail detail = getMediaArchive().getSearcher("moduleid").getDetail(inKey);
+				if (detail != null && detail.isMultiLanguage())
 				{
-					String inKey = (String) iterator2.next();
-					PropertyDetail detail = archive.getAssetPropertyDetails().getDetail(inKey);
-					if (detail != null && detail.isMultiLanguage())
+					LanguageMap value = data.getLanguageMap(inKey);
+					if (value != null && value.getText("en") != null && !value.getText("en").isEmpty())
 					{
-						
-						LanguageMap value = asset.getLanguageMap(inKey);
-						if (value != null && value.getText("en") != null && !value.getText("en").isEmpty())
-						{
-							fieldsmap.put(inKey, value);
-						}
-						
+						fieldsmap.put(inKey, value);
 					}
-				} 
+				}
+			} 
 
+			try
+			{
 				Map<String, LanguageMap> results = translateFields(fieldsmap, "en", targetLangs);
 
 				if(results != null)
@@ -389,37 +356,22 @@ public class TranslationManager implements CatalogEnabled {
 					{
 						String key = (String) iterator2.next();
 						LanguageMap map = results.get(key);
-						LanguageMap value = asset.getLanguageMap(key);
+						LanguageMap value = data.getLanguageMap(key);
 						value.putAll(map);
-						asset.setValue(key, value);
+						data.setValue(key, value);
 					}
-					inLog.info("Found translation for "+ asset.getId() + ", " + asset.getName());
+					inLog.info("Found translation for "+ data.getId() + ", " + data.getName());
 				}
-				
-				asset.setValue("translatesuccess", true);
-				tosave.add(asset);
 				long duration = (System.currentTimeMillis() - startTime);
-				inLog.info("Asset translation took: "+duration +"ms");
-				
+				inLog.info("translation took: "+duration +"ms");
 			} 
 			catch(Exception e){
 				inLog.error("Translation Error", e);
-				asset.setValue("translaterror", true);
-				tosave.add(asset);
 				continue;
 			}
-			if( tosave.size() == 25)	{
-				archive.saveAssets(tosave);
-				//searcher.saveAllData(tosave, null);
-				inLog.info("Saved: " + tosave.size() + " assets - " + searcher.getSearchType());
-				tosave.clear();
-			}
-			
 		}
-		archive.saveAssets(tosave);
-		inLog.info("Saved: " + tosave.size() + " assets - " + searcher.getSearchType());
-		tosave.clear();
+		
 	}
-	
+
   
 }
