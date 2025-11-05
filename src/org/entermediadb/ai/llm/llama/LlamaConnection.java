4b42920d4502fb98fc073b1e28ffb4f19caa9fd9
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-11-06 04:56:23 +0600

    Add structured output handling in LlamaConnection: implement callStructuredOutputList method for API interaction

--- src/org/entermediadb/ai/llm/llama/LlamaConnection.java
+++ src/org/entermediadb/ai/llm/llama/LlamaConnection.java
@@ -1,10 +1,18 @@
 package org.entermediadb.ai.llm.llama;
 
+
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.entermediadb.ai.llm.LlmResponse;
 import org.entermediadb.ai.llm.openai.OpenAiConnection;
-import org.entermediadb.ai.llm.openai.OpenAiResponse;
 import org.entermediadb.asset.MediaArchive;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -13,6 +21,8 @@ import org.openedit.page.Page;
 import org.openedit.util.JSONParser;
 
 public class LlamaConnection extends OpenAiConnection {
+	private static Log log = LogFactory.getLog(LlamaConnection.class);
+	
 	@Override
 	public String getModelIdentifier()
 	{
@@ -130,6 +140,71 @@ public class LlamaConnection extends OpenAiConnection {
 	    
 	    return response;
 
+	}
+	
+	@Override
+	public JSONObject callStructuredOutputList(String inStructureName, Map inParams)
+	{
+		MediaArchive archive = getMediaArchive();
+		
+		inParams.put("model", getModelIdentifier());
+		
+		
+		String structurepath = "/" + getMediaArchive().getMediaDbId() + "/ai/" + getLlmType() +"/classify/structures/" + inStructureName + "_structure.json";
+		Page defpage = archive.getPageManager().getPage(structurepath);
+		
+		if (defpage.exists())
+		{
+			String structure = loadInputFromTemplate(structurepath, inParams);
+			inParams.put("structure", structure);
+		}
+		
+		String prompt = loadInputFromTemplate("/" + getMediaArchive().getMediaDbId() + "/ai/" + getLlmType() +"/classify/structures/" + inStructureName + ".json", inParams);
+
+		JSONParser parser = new JSONParser();
+		JSONObject structureDef = (JSONObject) parser.parse(prompt);
+
+		String endpoint = getApiEndpoint();
+		HttpPost method = new HttpPost(endpoint);
+		method.addHeader("Authorization", "Bearer " + getApiKey());
+		method.setHeader("Content-Type", "application/json");
+		method.setEntity(new StringEntity(structureDef.toJSONString(), StandardCharsets.UTF_8));
+
+		CloseableHttpResponse resp = getConnection().sharedExecute(method);
+		
+		JSONObject results = new JSONObject();
+
+		try
+		{
+			if (resp.getStatusLine().getStatusCode() != 200)
+			{
+				throw new OpenEditException("GPT error: " + resp.getStatusLine());
+			}
+	
+			JSONObject json = (JSONObject) parser.parse(new StringReader(EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8)));
+
+			log.info("Returned: " + json.toJSONString());
+		
+		
+			JSONArray choices = (JSONArray) json.get("choices");
+	        JSONObject choice = (JSONObject) choices.get(0);
+	        JSONObject message = (JSONObject) choice.get("message");
+	        
+	        String contentString = (String) message.get("content");
+	        
+	        results = parser.parse(contentString);
+	        
+	        
+		}
+		catch (Exception e) 
+		{
+			throw new OpenEditException(e);
+		}
+		finally
+		{
+			connection.release(resp);
+		}
+		return results;
 	}
 	
 }
