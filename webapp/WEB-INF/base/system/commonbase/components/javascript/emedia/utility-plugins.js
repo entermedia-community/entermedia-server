201c96d80f00cbee9eeb22006b0cd39e49c958b3
Cristobal <cristobal.mejia@gmail.com>
2025-12-09 16:15:00 -0600

    NPE in Java Script

--- webapp/WEB-INF/base/system/commonbase/components/javascript/emedia/utility-plugins.js
+++ webapp/WEB-INF/base/system/commonbase/components/javascript/emedia/utility-plugins.js
@@ -292,10 +292,12 @@ getDropdownParent = function (theinput) {
 	if (parent.length) {
 		return parent;
 	}
-
-	var parent = $(".detail-" + theinput.data("searchtype"));
-	if (parent.length) {
-		return parent;
+	if(theinput.data("searchtype"))
+	{
+		var parent = $(".detail-" + theinput.data("searchtype"));
+		if (parent.length) {
+			return parent;
+		}
 	}
 
 	var inmodal = theinput.closest(".modal");
