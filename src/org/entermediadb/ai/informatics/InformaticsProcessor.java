a32268b5c5c1b0f1679bed8f58a358a56d5a0c92
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-01-15 21:33:23 +0600

    Refactor DocumentSplitterManager to enhance image loading and error handling

--- src/org/entermediadb/ai/informatics/InformaticsProcessor.java
+++ src/org/entermediadb/ai/informatics/InformaticsProcessor.java
@@ -2,15 +2,11 @@ package org.entermediadb.ai.informatics;
 
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.entermediadb.ai.BaseAiManager;
 import org.entermediadb.ai.classify.ClassifyManager;
-import org.entermediadb.asset.Asset;
-import org.entermediadb.asset.convert.ConvertResult;
 import org.entermediadb.scripts.ScriptLogger;
 import org.openedit.MultiValued;
 
@@ -21,43 +17,4 @@ public abstract class InformaticsProcessor extends BaseAiManager
 	public abstract void processInformaticsOnAssets(ScriptLogger inLog,MultiValued inConfig, Collection<MultiValued> assets );
 	public abstract void processInformaticsOnEntities(ScriptLogger inLog,MultiValued inConfig, Collection<MultiValued> records );
 	
-	
-	protected String loadDocumentContent(MultiValued inEntity)
-	{
-		boolean isDocPage = inEntity.get("entitydocument") != null;
-
-		String base64EncodedString = null;
-		if(isDocPage && inEntity.hasValue("pagenum") )
-		{
-			String parentasset = inEntity.get("parentasset");
-			if(parentasset != null)
-			{
-				Asset parentAsset = getMediaArchive().getAsset(parentasset);
-				//Do the conversion with page number in it
-				Map params = new HashMap();
-				params.put("pagenum",inEntity.get("pagenum") );
-				ConvertResult result = getMediaArchive().getTranscodeTools().createOutputIfNeeded(null,params,parentAsset.getSourcePath(), "image3000x3000.webp"); 
-				if( result.isOk() )
-				{
-					base64EncodedString = loadBase64Png(result.getOutput());
-				}
-			}
-		}
-		else
-		{
-			String primarymedia = inEntity.get("primarymedia");
-			Asset inPrimaryAsset = getMediaArchive().getAsset(primarymedia);
-			if(inPrimaryAsset == null)
-			{
-				primarymedia = inEntity.get("primaryimage");
-				inPrimaryAsset = getMediaArchive().getAsset(primarymedia);
-			}
-			if(inPrimaryAsset != null)
-			{
-				base64EncodedString = loadBase64Png(inPrimaryAsset, "image3000x3000");
-			}
-		}
-		return base64EncodedString;
-	}
-
 }
