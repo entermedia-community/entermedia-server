5a2989cea1619af0364907acfbd9c87361e16859
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-02-20 00:02:27 +0600

    Update logging to output payload directly in SmartCreatorManager

--- src/org/entermediadb/ai/creator/SmartCreatorManager.java
+++ src/org/entermediadb/ai/creator/SmartCreatorManager.java
@@ -800,7 +800,7 @@ public class SmartCreatorManager extends BaseAiManager implements ChatMessageHan
 			
 			payload.put("parent_ids", parentIds);
 			
-			log.info("sending to server: " +  payload.toJSONString());
+			log.info("sending to server: " +  payload);
 
 			LlmResponse res = llmconnection.callJson("/query", payload);
 			
