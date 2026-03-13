42b130f8cc10b110b67a8cf3876e2f337fe34675
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-03-13 23:06:40 +0600

    Fix typo in loadScenarios method and enhance loadAutomationScenarios functionality

--- src/org/entermediadb/ai/automation/AutomationModule.java
+++ src/org/entermediadb/ai/automation/AutomationModule.java
@@ -2,7 +2,10 @@ package org.entermediadb.ai.automation;
 
 import java.util.Collection;
 
+import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.modules.BaseMediaModule;
+import org.openedit.Data;
+import org.openedit.MultiValued;
 import org.openedit.WebPageRequest;
 
 public class AutomationModule extends BaseMediaModule {
@@ -10,9 +13,27 @@ public class AutomationModule extends BaseMediaModule {
 
     public void loadScenarios(WebPageRequest inRequest)
     {
-        Collection list = getMediaArchive(inRequest).getList("automationscenerio");
+        Collection list = getMediaArchive(inRequest).getList("automationscenario");
         inRequest.putPageValue("scenarios", list);
 
     }
+    
+    public void loadAutomationScenarios(WebPageRequest inReq)
+	{
+    	MediaArchive archive = getMediaArchive(inReq);
+    	
+    	String scenarioid = inReq.getRequestParameter("scenarioid");
+    	
+    	if(scenarioid == null)
+		{
+			return;
+		}
+    	
+    	Data scenario = archive.query("automationscenario").exact("id", scenarioid).searchOne();
+    	inReq.putPageValue("scenario", scenario);
+    	
+    	Collection<MultiValued> agents = archive.query("automationagentenabled").exact("automationscenario", scenario.getId()).search();
+    	inReq.putPageValue("agents", agents);
+	}
 
 }
