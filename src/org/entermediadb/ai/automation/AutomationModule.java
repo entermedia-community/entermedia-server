649621e3d69918035bd109e597bf5daad8f42cb6
Christopher Burkey <cburkey@entermediadb.org>
2026-03-18 10:45:12 -0600

    Upload code

--- src/org/entermediadb/ai/automation/AutomationModule.java
+++ src/org/entermediadb/ai/automation/AutomationModule.java
@@ -2,16 +2,25 @@ package org.entermediadb.ai.automation;
 
 import java.util.Collection;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.entermediadb.ai.llm.AgentContext;
+import org.entermediadb.asset.Asset;
 import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.modules.BaseMediaModule;
+import org.entermediadb.asset.modules.ConvertStatusModule;
+import org.entermediadb.asset.upload.FileUpload;
+import org.entermediadb.asset.upload.UploadRequest;
 import org.entermediadb.scripts.ScriptLogger;
 import org.openedit.Data;
 import org.openedit.MultiValued;
 import org.openedit.WebPageRequest;
 import org.openedit.data.Searcher;
+import org.openedit.repository.ContentItem;
+import org.openedit.util.PathUtilities;
 
 public class AutomationModule extends BaseMediaModule {
+	private static final Log log = LogFactory.getLog(AutomationModule.class);
 
 	public void loadScenarios(WebPageRequest inReq)
 	{
@@ -100,6 +109,34 @@ public class AutomationModule extends BaseMediaModule {
 		
 	}
 	
+
+	public void saveAutomationSnapshot(WebPageRequest inReq)
+	{
+		//Regular upload
+		MediaArchive archive = getMediaArchive(inReq);
+		FileUpload command = (FileUpload) archive.getBean("fileUpload");
+		UploadRequest properties = command.parseArguments(inReq);
+		
+		if (properties == null) {
+			return;
+		}
+		if (properties.getFirstItem() == null) 
+		{
+			log.info("No upload found");
+			return;
+		}
+		String apphome = inReq.findValue("apphome");
+		String automationid = inReq.getRequestParameter("automationid");
+		String sourcepath = apphome + "/views/automations/" + automationid + ".png" ;
+		//TODO: Check for formats
+		
+		//Save to temp place to change format
+		String tmpplace = "/WEB-INF/trash/" + archive.getCatalogId()	+ "/originals/" + sourcepath;
+		ContentItem tosave = archive.getPageManager().getRepository().getStub(tmpplace);
+		
+		ContentItem saved = properties.saveFileAs(properties.getFirstItem(), tosave, inReq.getUser());
+
+	}
 	
 
 }
