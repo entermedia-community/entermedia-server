5f38ea1f8e1617fc3ecdd36a7b22fb35c56162fd
Christopher Burkey <cburkey@entermediadb.org>
2026-03-19 12:00:32 -0600

    start vs run

--- src/org/entermediadb/ai/automation/RunEventAgent.java
+++ src/org/entermediadb/ai/automation/RunEventAgent.java
@@ -1,17 +1,9 @@
 package org.entermediadb.ai.automation;
 
-import java.util.Iterator;
-import java.util.Map;
-
-import org.entermediadb.ai.BaseAgent;
 import org.entermediadb.ai.llm.AgentContext;
-import org.entermediadb.events.PathEventManager;
-import org.openedit.BaseWebPageRequest;
-import org.openedit.MultiValued;
 import org.openedit.WebPageRequest;
-import org.openedit.page.PageRequestKeys;
 
-public class RunEventAgent extends BaseAgent
+public class RunEventAgent extends StartEventAgent
 {
 	
 	/**
@@ -22,50 +14,10 @@ public class RunEventAgent extends BaseAgent
 	@Override
 	public void process(AgentContext inContext)
 	{
+		String operation = inContext.getCurrentAgentEnable().getAgentConfig().getId();
 		WebPageRequest request =  (WebPageRequest)inContext.getContextValue("webpagerequest");
-		
-		if(request != null)
-		{
-			Map params = request.getParameterMap();
-			//Map values = getRequestUtils().extractValueMap(request);
-			inContext.put("parameters", params);
-			
-			String entityid = request.getRequestParameter("entityid");
-			String entitymoduleid = request.getRequestParameter("entitymoduleid");
-
-			
-			MultiValued entity = (MultiValued)getMediaArchive().getCachedData(entitymoduleid, entityid);
-			inContext.put("currententity",entity);
-			
-			MultiValued entitymodule = (MultiValued)getMediaArchive().getCachedData("module",entitymoduleid);
-			inContext.put("currententitymodule",entitymodule);
-			
-			String triggerapplicationid = (String) request.getPageValue("triggerapplicationid");
-			inContext.put("triggerapplicationid", triggerapplicationid);
-			inContext.put("triggersiteroot", request.getSiteRoot());
-			
-			inContext.setUserProfile(request.getUserProfile());
-			
-			request.putPageValue("currentagentcontext",inContext);
-			
-			String operation = inContext.getCurrentAgentEnable().getAgentConfig().get("operation");
-			if( operation != null)
-			{
-				runPathEvent(inContext, operation, request);
-			}
-		}
-		super.process(inContext);
+		runPathEvent(inContext, operation, request);
 	}
 
-	private void runPathEvent(AgentContext inContext, String operation, WebPageRequest inRequest)
-	{
-		String runpath = "/" + getCatalogId() + "/events/" + operation + ".html";
-		PathEventManager manager = (PathEventManager) getModuleManager().getBean(getCatalogId(), "pathEventManager");
-		WebPageRequest request = new BaseWebPageRequest(inRequest);
-
-		request.setRequestParameter("catalogid", getCatalogId());
-		manager.runPathEvent(runpath, request);
-		
-	}
 	
 }
---
+++ src/org/entermediadb/ai/automation/StartEventAgent.java
@@ -0,0 +1,77 @@
+package org.entermediadb.ai.automation;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.entermediadb.ai.BaseAgent;
+import org.entermediadb.ai.llm.AgentContext;
+import org.entermediadb.events.PathEventManager;
+import org.openedit.BaseWebPageRequest;
+import org.openedit.MultiValued;
+import org.openedit.WebPageRequest;
+import org.openedit.page.PageRequestKeys;
+
+public class StartEventAgent extends BaseAgent
+{
+	
+	/**
+	 * Calls render to html
+	 * Attaches and asset version
+	 * sends it to a blog?
+	 */
+	@Override
+	public void process(AgentContext inContext)
+	{
+		WebPageRequest request =  (WebPageRequest)inContext.getContextValue("webpagerequest");
+		
+		if(request != null)
+		{
+			Map params = request.getParameterMap();
+			//Map values = getRequestUtils().extractValueMap(request);
+			inContext.put("parameters", params);
+			
+			String entityid = request.getRequestParameter("entityid");
+			String entitymoduleid = request.getRequestParameter("entitymoduleid");
+
+			
+			MultiValued entity = (MultiValued)getMediaArchive().getCachedData(entitymoduleid, entityid);
+			inContext.put("currententity",entity);
+			
+			MultiValued entitymodule = (MultiValued)getMediaArchive().getCachedData("module",entitymoduleid);
+			inContext.put("currententitymodule",entitymodule);
+			
+			String triggerapplicationid = (String) request.getPageValue("triggerapplicationid");
+			inContext.put("triggerapplicationid", triggerapplicationid);
+			inContext.put("triggersiteroot", request.getSiteRoot());
+			
+			inContext.setUserProfile(request.getUserProfile());
+			
+			request.putPageValue("currentagentcontext",inContext);
+			
+			String operation = inContext.getCurrentAgentEnable().getAgentConfig().getId();
+			runPathEvent(inContext, operation, request);
+		}
+		else
+		{
+			super.process(inContext);
+		}
+	}
+
+	protected void runPathEvent(AgentContext inContext, String operation, WebPageRequest inRequest)
+	{
+		AgentContext child = new AgentContext(inContext);
+		String runpath = "/" + getCatalogId() + "/events/" + operation + ".html";
+		PathEventManager manager = (PathEventManager) getModuleManager().getBean(getCatalogId(), "pathEventManager");
+		WebPageRequest request = new BaseWebPageRequest(inRequest);
+
+		child.put("webpagerequest",request); //TODO: Make this one combined context
+		request.putPageValue("currentagentcontext",child);
+		
+		request.setRequestParameter("catalogid", getCatalogId());
+		manager.runPathEvent(runpath, request);
+		super.process(child);
+	
+	}
+
+	
+}
