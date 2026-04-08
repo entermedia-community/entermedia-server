f3787ccf9833c687a88b7042a9221bbd06b69e59
Cristobal <cristobal.mejia@gmail.com>
2026-04-08 17:35:55 -0600

    Wrong absolute path for Docx

--- src/org/entermediadb/ooffice/OofficeDocumentTranscoder.java
+++ src/org/entermediadb/ooffice/OofficeDocumentTranscoder.java
@@ -19,59 +19,63 @@ import org.openedit.util.PathUtilities;
 
 public class OofficeDocumentTranscoder extends BaseTranscoder
 {
-//	protected final def formats = ["doc","docx","rtf","ppt","pptx","wps","odt","html","xml","csv", "xls", "xlsx", "odp"];
+	// protected final def formats =
+	// ["doc","docx","rtf","ppt","pptx","wps","odt","html","xml","csv", "xls", "xlsx", "odp"];
 	private static final Log log = LogFactory.getLog(OofficeDocumentTranscoder.class);
-	
+
 	public synchronized ConvertResult convert(ConvertInstructions inStructions)
 	{
 		MediaArchive inArchive = inStructions.getMediaArchive();
 		Asset inAsset = inStructions.getAsset();
 		ContentItem inOut = inStructions.getOutputFile();
-		
+
 		ConvertResult result = new ConvertResult();
 		result.setOutput(inOut);
 		result.setOk(false);
-		
-		Page input = inArchive.findOriginalMediaByType("document",inAsset);
-		if( input == null )
+
+		Page input = inArchive.findOriginalMediaByType("document", inAsset);
+		if (input == null)
 		{
 			return result;
 		}
 
 		List command = new ArrayList();
-		
+
 		command.add("-invisible");
 		command.add("-headless");
 		command.add("-nologo");
-		command.add("-norestore");		
+		command.add("-norestore");
 		command.add("-nolockcheck");
-		
-		command.add("-convert-to");		
+
+		command.add("-convert-to");
 		command.add("pdf");
 
 		command.add("-outdir");
-		
+
 		Page out = getPageManager().getPage(inOut.getPath());
 		String outfolder = out.getDirectory();
 		String dir = out.getDirectory();
 		dir = getPageManager().getPage(dir).getContentItem().getAbsolutePath();
-		//log.info("{$inOut} turns into ${dir}");
-		//String dir = inStructions.getOutputFile().getAbsolutePath();
-		new File( dir ).mkdirs();
+		// log.info("{$inOut} turns into ${dir}");
+		// String dir = inStructions.getOutputFile().getAbsolutePath();
+		new File(dir).mkdirs();
 		command.add(dir);
-		
-		command.add(input.getContentItem().getAbsolutePath());
-		
+
+		String absolutePath = inStructions.getInputFile().getAbsolutePath();
+
+		// command.add(input.getContentItem().getAbsolutePath());
+		command.add(absolutePath);
+
 		long timeout = inStructions.getConversionTimeout();
-		ExecResult done = getExec().runExec("soffice",command, timeout);
-		
+		ExecResult done = getExec().runExec("soffice", command, timeout);
+
 		result.setOk(done.isRunOk());
-		if( done.isRunOk() )
+		if (done.isRunOk())
 		{
 			String newname = PathUtilities.extractPageName(input.getName()) + ".pdf";
-			
-			Page tmpfile = getPageManager().getPage(outfolder+ "/" + newname);
-			if( !tmpfile.exists() || tmpfile.length() == 0)
+
+			Page tmpfile = getPageManager().getPage(outfolder + "/" + newname);
+			if (!tmpfile.exists() || tmpfile.length() == 0)
 			{
 				throw new OpenEditException("OpenOffice did not create output file " + tmpfile);
 			}
@@ -82,41 +86,42 @@ public class OofficeDocumentTranscoder extends BaseTranscoder
 		}
 		else
 		{
-			log.error("Error running command on : " + input.getName() + " output:" + done.getStandardOut()  + " returned: " + done.getReturnValue() );
+			log.error("Error running command on : " + input.getName() + " output:"
+					+ done.getStandardOut() + " returned: " + done.getReturnValue());
 		}
-	    return result;
+		return result;
 	}
 
-//	public String populateOutputPath(MediaArchive inArchive, ConvertInstructions inStructions)
-//	{
-//		//we only generate PDF for now
-//		StringBuffer path = new StringBuffer();
-//
-//		//legacy for people who want to keep their images in the old location
-//		String prefix = inStructions.getProperty("pathprefix");
-//		if( prefix != null)
-//		{
-//			path.append(prefix);
-//		}
-//		else
-//		{
-//			path.append("/WEB-INF/data");
-//			path.append(inArchive.getCatalogHome());
-//			path.append("/generated/");
-//		}
-//		path.append(inStructions.getAssetSourcePath());
-//		path.append("/");
-//
-//		String postfix = inStructions.getProperty("pathpostfix");
-//		if( postfix != null)
-//		{
-//			path.append(postfix);
-//		}	
-//		inStructions.setOutputExtension("pdf");
-//		path.append("document." + inStructions.getOutputExtension());
-//
-//		inStructions.setOutputPath(path.toString());
-//		return path.toString();
-//	}
+	// public String populateOutputPath(MediaArchive inArchive, ConvertInstructions inStructions)
+	// {
+	// //we only generate PDF for now
+	// StringBuffer path = new StringBuffer();
+	//
+	// //legacy for people who want to keep their images in the old location
+	// String prefix = inStructions.getProperty("pathprefix");
+	// if( prefix != null)
+	// {
+	// path.append(prefix);
+	// }
+	// else
+	// {
+	// path.append("/WEB-INF/data");
+	// path.append(inArchive.getCatalogHome());
+	// path.append("/generated/");
+	// }
+	// path.append(inStructions.getAssetSourcePath());
+	// path.append("/");
+	//
+	// String postfix = inStructions.getProperty("pathpostfix");
+	// if( postfix != null)
+	// {
+	// path.append(postfix);
+	// }
+	// inStructions.setOutputExtension("pdf");
+	// path.append("document." + inStructions.getOutputExtension());
+	//
+	// inStructions.setOutputPath(path.toString());
+	// return path.toString();
+	// }
 
 }
