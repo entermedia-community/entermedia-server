be13fddb9a5e132639a86d8e719389cb51296c03
Christopher Burkey <cburkey@entermediadb.org>
2026-04-20 17:50:27 -0600

    Added agents

--- src/org/entermediadb/ai/automation/agents/RunConversionsAgent.java
+++ src/org/entermediadb/ai/automation/agents/RunConversionsAgent.java
@@ -3,23 +3,20 @@ package org.entermediadb.ai.automation.agents;
 import org.entermediadb.ai.BaseAgent;
 import org.entermediadb.ai.llm.AgentContext;
 import org.entermediadb.asset.convert.QueueManager;
-import org.entermediadb.scripts.ScriptLogger;
 
-public class ProcessConversions extends BaseAgent
+public class RunConversionsAgent extends BaseAgent
 {
 	@Override
 	public void process(AgentContext inContext)
 	{
+		// TODO: Use a single thread for this?
 
-		QueueManager queueManager = (QueueManager) getModuleManager()
-				.getBean(getMediaArchive().getCatalogId(), "queueManager");
+		QueueManager queueManager = (QueueManager) getModuleManager().getBean(getMediaArchive().getCatalogId(), "queueManager");
 		queueManager.checkQueue();
 
 		if (queueManager.getTotalPending() > 0)
 		{
-			inContext.info("Total Pending Tasks: " + queueManager.getTotalPending()
-					+ " Threads running: " + queueManager.runningProcesses() + " of "
-					+ queueManager.getMaxProcessors());
+			inContext.info("Total Pending Tasks: " + queueManager.getTotalPending() + " Threads running: " + queueManager.runningProcesses() + " of " + queueManager.getMaxProcessors());
 		}
 
 		if (queueManager.getRunningAssetIds().size() > 1)
