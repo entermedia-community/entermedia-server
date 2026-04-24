55d62cdcb72a960b7ca85cf376aa4e5c93fbcb5a
Cristobal <cristobal.mejia@gmail.com>
2026-04-24 14:32:37 -0600

    Expand tree after save node

--- src/org/entermediadb/webui/tree/BaseTreeRenderer.java
+++ src/org/entermediadb/webui/tree/BaseTreeRenderer.java
@@ -1,14 +1,14 @@
 /*
-Copyright (c) 2003 eInnovation Inc. All rights reserved
-
-This library is free software; you can redistribute it and/or modify it under the terms
-of the GNU Lesser General Public License as published by the Free Software Foundation;
-either version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-*/
+ * Copyright (c) 2003 eInnovation Inc. All rights reserved
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
 
 /*
  * Created on May 29, 2003
@@ -39,30 +39,25 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 
 	protected String fieldUrlPrefix = "";
 	protected String fieldUrlPostfix = "";
-	protected boolean  fieldFoldersLinked;
+	protected boolean fieldFoldersLinked;
 	protected boolean fieldRenderLeaves = true;
 	protected boolean fieldAllowSelections = false;
-	protected Set fieldSelectedNodes; 
+	protected Set fieldSelectedNodes;
 	protected boolean fieldEditable;
-	
-	
+
 	public boolean isEditable()
 	{
 		return fieldEditable;
 	}
-
 
 	public void setEditable(boolean inEditable)
 	{
 		fieldEditable = inEditable;
 	}
 
+	public BaseTreeRenderer() {
 
-	public BaseTreeRenderer()
-	{
-		
 	}
-
 
 	public boolean isAllowSelections()
 	{
@@ -74,7 +69,6 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 		fieldAllowSelections = inAllowSelections;
 	}
 
-	
 	public Set getSelectedNodes()
 	{
 		if (fieldSelectedNodes == null)
@@ -83,19 +77,22 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 		}
 		return fieldSelectedNodes;
 	}
+
 	public boolean isIdSelected(String inNodeId)
 	{
 		return getSelectedNodes().contains(inNodeId); // TODO: look at this
 	}
+
 	public boolean isNodeSelected(Object inNode)
 	{
-		if( inNode == getSelectedNode() )
+		if (inNode == getSelectedNode())
 		{
 			return true;
 		}
 		String inId = getId(inNode);
 		return getSelectedNodes().contains(inId);
 	}
+
 	protected String getId(Object inNode)
 	{
 		return getWebTree().getModel().getId(inNode);
@@ -104,42 +101,59 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	public void setSelectedNodes(Set inSelectedNodes)
 	{
 		fieldSelectedNodes = inSelectedNodes;
-		if(inSelectedNodes == null)
+		if (inSelectedNodes == null)
 		{
 			setSelectedNode(null);
 		}
 	}
+
 	public void selectNode(Object inNode)
 	{
-		if( inNode != null )
+		if (inNode != null)
 		{
 			getSelectedNodes().clear();
 			getSelectedNodes().add(getId(inNode));
-			
+
 			Object parent = getWebTree().getModel().getParent(inNode);
-			if( parent != null )
+			if (parent != null)
 			{
 				expandNode(parent);
 			}
 		}
 		fieldSelectedNode = inNode;
 	}
+
 	public void unSelectNode(Object inNode)
 	{
 		getSelectedNodes().remove(getId(inNode));
 	}
+
+	public void selectNodes(String inNode)
+	{
+		if (inNode == null)
+		{
+			return;
+		}
+		Object target = getWebTree().getModel().getChildById(inNode);
+		if (target != null)
+		{
+			expandNode(target);
+			setSelectedNode(target);
+		}
+	}
+
 	public void selectNodes(Collection inNodes)
 	{
 		setSelectedNode(null);
 		Set newselection = new HashSet();
-		if(inNodes != null)
+		if (inNodes != null)
 		{
 			for (Iterator iterator = inNodes.iterator(); iterator.hasNext();)
 			{
 				Object object = (Object) iterator.next();
 				newselection.add(getId(object));
 				Object parent = getWebTree().getModel().getParent(object);
-				if( parent != null )
+				if (parent != null)
 				{
 					expandNode(parent);
 				}
@@ -147,25 +161,26 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 			}
 		}
 		setSelectedNodes(newselection);
-		
+
 	}
 
-	
 	public boolean isRenderLeaves()
 	{
 		return fieldRenderLeaves;
 	}
+
 	public void setRenderLeaves(boolean inRenderLeaves)
 	{
 		fieldRenderLeaves = inRenderLeaves;
 	}
-	public BaseTreeRenderer(WebTree inWebTree)
-	{
+
+	public BaseTreeRenderer(WebTree inWebTree) {
 		fieldWebTree = inWebTree;
 	}
+
 	public String getHome()
 	{
-		if ( fieldHome == null )
+		if (fieldHome == null)
 		{
 			fieldHome = "";
 		}
@@ -186,10 +201,12 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	{
 		return fieldImageDir;
 	}
+
 	public void setImageDir(String inImageDir)
 	{
 		fieldImageDir = inImageDir;
 	}
+
 	public void setWebTree(WebTree tree)
 	{
 		fieldWebTree = tree;
@@ -199,6 +216,7 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	{
 		return fieldWebTree;
 	}
+
 	protected Set getExpandedNodes()
 	{
 		if (fieldExpandedNodes == null)
@@ -212,9 +230,9 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	@Override
 	public void toggleNode(Object inNode)
 	{
-		if  ( inNode != null) 
+		if (inNode != null)
 		{
-			if( hasBeenExpanded(inNode) )
+			if (hasBeenExpanded(inNode))
 			{
 				collapseNode(inNode);
 			}
@@ -224,26 +242,29 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 			}
 		}
 	}
+
 	public void expandNode(Object inNode)
 	{
-		if( inNode == null)
+		if (inNode == null)
 		{
 			return;
 		}
 		String path = toUrl(inNode);
-		if ( !getExpandedNodes().contains(path))
+		if (!getExpandedNodes().contains(path))
 		{
 			getExpandedNodes().add(path);
-			//get all the parents somehow
-			Object parent = getWebTree().getModel().getParent( inNode);
-			if ( parent != null)
+			// get all the parents somehow
+			Object parent = getWebTree().getModel().getParent(inNode);
+			if (parent != null)
 			{
 				expandNode(parent);
 			}
 		}
 	}
-	
-	/* (non-javadoc)
+
+	/*
+	 * (non-javadoc)
+	 * 
 	 * @see org.entermediadb.webui.tree.TreeRenderer#collapseNode(java.lang.Object)
 	 */
 	public void collapseNode(Object inNode)
@@ -255,16 +276,16 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 
 	public boolean hasBeenExpanded(Object inNode)
 	{
-		//Make sure it is expanded
+		// Make sure it is expanded
 		Object parent = inNode;
-		while( parent != null)
+		while (parent != null)
 		{
 			String path = toUrl(inNode);
-			if( getExpandedNodes().contains(path) )
+			if (getExpandedNodes().contains(path))
 			{
 				parent = getWebTree().getModel().getParent(parent);
-				//If we get to the root and it is selected still then we are ok!
-				if( parent == null)
+				// If we get to the root and it is selected still then we are ok!
+				if (parent == null)
 				{
 					return true;
 				}
@@ -282,72 +303,74 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 		inString = inString.replace("\"", "\\\"");
 		return "\"" + inString + "\"";
 	}
-	
+
 	public String renderAsString()
 	{
-		StringBuffer js = new StringBuffer(  "var tree = new Tree( \n" );
-		
-		// Ensure the root node is always expanded.
-		expandNode( getWebTree().getModel().getRoot() );
+		StringBuffer js = new StringBuffer("var tree = new Tree( \n");
 
-		js.append( renderNewFolder( getWebTree().getModel().getRoot()));
-		js.append( ", " );
-		js.append( quotes(getWebTree().getId()) );
-		js.append( ", \"" );
-		js.append(  getHome() );
-		js.append( getImageDir() + "/\" );\n");
-		if ( getUrlPrefix() != null)
+		// Ensure the root node is always expanded.
+		expandNode(getWebTree().getModel().getRoot());
+
+		js.append(renderNewFolder(getWebTree().getModel().getRoot()));
+		js.append(", ");
+		js.append(quotes(getWebTree().getId()));
+		js.append(", \"");
+		js.append(getHome());
+		js.append(getImageDir() + "/\" );\n");
+		if (getUrlPrefix() != null)
 		{
-			js.append( "tree.setViewerUrl( '" + getHome() + getUrlPrefix() + "' );\n" );
+			js.append("tree.setViewerUrl( '" + getHome() + getUrlPrefix() + "' );\n");
 		}
-		if ( getUrlPostfix() != null)
+		if (getUrlPostfix() != null)
 		{
-			js.append( "tree.setViewerUrlEnding( '" + getUrlPostfix() + "' );\n" );
+			js.append("tree.setViewerUrlEnding( '" + getUrlPostfix() + "' );\n");
 		}
-		js.append( "tree.showFolderData= " + isFoldersLinked() + ";\n");
-		if ( getSelectedNode() != null)
+		js.append("tree.showFolderData= " + isFoldersLinked() + ";\n");
+		if (getSelectedNode() != null)
 		{
-			js.append("tree.setSelectedNodeById(" );
-			js.append(quotes(getWebTree().getModel().getId( getSelectedNode() ) ));
+			js.append("tree.setSelectedNodeById(");
+			js.append(quotes(getWebTree().getModel().getId(getSelectedNode())));
 			js.append(");\n");
 		}
 		return js.toString();
 	}
+
 	public String renderAsString(String inNodeId)
 	{
 		return null;
 	}
+
 	public Object setSelectedNodeByUrl(String inPath)
 	{
-		if ( inPath == null)
+		if (inPath == null)
 		{
 			return null;
 		}
-		
-		if ( inPath.endsWith("/") && inPath.length() > 1 )
+
+		if (inPath.endsWith("/") && inPath.length() > 1)
 		{
 			inPath = inPath.substring(0, inPath.length() - 1);
 		}
 
 		Object node = null;
-		
-		String root = toUrl( getWebTree().getModel().getRoot() );
-		if ( PathUtilities.match(root, inPath ) )
+
+		String root = toUrl(getWebTree().getModel().getRoot());
+		if (PathUtilities.match(root, inPath))
 		{
 			node = getWebTree().getModel().getRoot();
 		}
 		else
-		{		
+		{
 			node = findNodeByUrl(getWebTree().getModel().getRoot(), inPath);
 		}
 		setSelectedNode(node);
-		if  ( node != null)
+		if (node != null)
 		{
-			//expand the parent
+			// expand the parent
 			Object parent = getWebTree().getModel().getParent(node);
-			if ( parent != null)
+			if (parent != null)
 			{
-				expandNode(parent);				
+				expandNode(parent);
 			}
 		}
 		return node;
@@ -355,63 +378,62 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 
 	public Object findNodeByUrl(Object inRoot, String inUrl)
 	{
-		if(inUrl == null){
+		if (inUrl == null)
+		{
 			return null;
 		}
 		int count = getWebTree().getModel().getChildCount(inRoot);
 		for (int i = 0; i < count; i++)
 		{
-			Object child = getWebTree().getModel().getChild(inRoot,i);
-			
-			String test = toUrl( child );
-			if ( PathUtilities.match(test,inUrl  ) )
+			Object child = getWebTree().getModel().getChild(inRoot, i);
+
+			String test = toUrl(child);
+			if (PathUtilities.match(test, inUrl))
 			{
 				return child;
 			}
 
-			//this test must be a directory
-			if( !inUrl.startsWith(test))
+			// this test must be a directory
+			if (!inUrl.startsWith(test))
 			{
 				continue;
 			}
-			child = findNodeByUrl(child,inUrl);
-			if ( child != null)
+			child = findNodeByUrl(child, inUrl);
+			if (child != null)
 			{
 				return child;
 			}
 		}
 		return null;
 	}
-	
+
 	public void setSelectedNodeById(String inId)
 	{
-		if ( inId == null)
+		if (inId == null)
 		{
 			return;
 		}
 		Object node = getWebTree().getModel().getChildById(inId);
 		setSelectedNode(node);
-		if  ( node != null)
+		if (node != null)
 		{
-			//expand the parent
+			// expand the parent
 			node = getWebTree().getModel().getParent(node);
-			if ( node != null)
+			if (node != null)
 			{
-				expandNode(node);				
+				expandNode(node);
 			}
 		}
 
 	}
 
-	
 	protected String renderNewFolder(Object inNode)
 	{
-		StringBuffer js = new StringBuffer(
-				"new Node( " + quotes( toName( inNode ) ) + ", " + quotes( toUrl( inNode ) ) + ", ");
+		StringBuffer js = new StringBuffer("new Node( " + quotes(toName(inNode)) + ", " + quotes(toUrl(inNode)) + ", ");
 
 		js.append(quotes(getWebTree().getModel().getId(inNode)));
 
-		if ( getWebTree().getModel().isLeaf( inNode ) )
+		if (getWebTree().getModel().isLeaf(inNode))
 		{
 			js.append(",null");
 		}
@@ -420,10 +442,10 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 			js.append(renderChildren(inNode));
 		}
 
-		js.append( "," + getWebTree().getModel().isLeaf( inNode ) );
-		
+		js.append("," + getWebTree().getModel().isLeaf(inNode));
+
 		String iconsset = customIconSet(inNode);
-		if( iconsset != null )
+		if (iconsset != null)
 		{
 			js.append(',');
 			js.append("\"" + iconsset + "\")");
@@ -435,8 +457,10 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 
 		return js.toString();
 	}
+
 	/**
 	 * Allows a certain node to have a custom icon set. Icon sets can be defined in the tree.js
+	 * 
 	 * @param inNode
 	 * @return
 	 */
@@ -444,28 +468,29 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	{
 		return null;
 	}
+
 	protected String renderChildren(Object inNode)
 	{
-		//	Use null for our folder icon, and the JavaScript tree will substitute either folder.gif or
-		//	folderopen.gif, depending on whether the node is open or closed.
+		// Use null for our folder icon, and the JavaScript tree will substitute either folder.gif or
+		// folderopen.gif, depending on whether the node is open or closed.
 		StringBuffer js = new StringBuffer();
-		
-		if ( hasBeenExpanded( inNode ) )
+
+		if (hasBeenExpanded(inNode))
 		{
-//			String collapseUrl = quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
-//				getCollapseNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode));
-//			js.append(collapseUrl +", new Array(\n");
+			// String collapseUrl = quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
+			// getCollapseNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode));
+			// js.append(collapseUrl +", new Array(\n");
 
 			js.append(", new Array(\n");
 
-			int c = getWebTree().getModel().getChildCount( inNode );
-			for (int i = 0; i < c; i++ )
+			int c = getWebTree().getModel().getChildCount(inNode);
+			for (int i = 0; i < c; i++)
 			{
-				if ( i > 0 )
+				if (i > 0)
 				{
 					js.append(",\n");
 				}
-				Object childNode = getWebTree().getModel().getChild( inNode, i );
+				Object childNode = getWebTree().getModel().getChild(inNode, i);
 				js.append(renderNewFolder(childNode));
 			}
 
@@ -473,31 +498,33 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 		}
 		else
 		{
-//			String expandUrl = quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
-//				getExpandNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode));
+			// String expandUrl = quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
+			// getExpandNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode));
 
 			// Might need to make this configurable later on.
 			// Different controllers may want to use different commands here.
-			js.append(",null " );
-			
-		//				js.append(
-		//		quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
-		//			getExpandNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode)) + ", null, ");
-			
+			js.append(",null ");
+
+			// js.append(
+			// quotes("?" + "WebTreeName=" + getWebTree().getName() + "&oe-action=" +
+			// getExpandNodeCommand() + "&nodeID=" + getWebTree().getModel().getId(inNode)) + ", null, ");
+
 		}
 
 		return js.toString();
 	}
-	
+
 	/**
 	 * The name is used on the tree display
+	 * 
 	 * @param inNode
 	 * @return
 	 */
 	public abstract String toName(Object inNode);
-	
+
 	/**
 	 * The url will be triggered when a node is clicked
+	 * 
 	 * @param inNode
 	 * @return
 	 */
@@ -512,14 +539,17 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	{
 		return fieldUrlPrefix;
 	}
+
 	public void setUrlPrefix(String inUrlPrefix)
 	{
 		fieldUrlPrefix = inUrlPrefix;
 	}
+
 	public boolean isFoldersLinked()
 	{
 		return fieldFoldersLinked;
 	}
+
 	public void setFoldersLinked(boolean inFoldersLinked)
 	{
 		fieldFoldersLinked = inFoldersLinked;
@@ -529,70 +559,75 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 	{
 		return fieldSelectedNode;
 	}
-	
+
 	public String getSelectedId()
 	{
 		Object selected = getSelectedNode();
-		if( selected == null)
+		if (selected == null)
 		{
 			return null;
 		}
 		return getWebTree().getModel().getId(selected);
 	}
-	
+
 	public void setSelectedNode(Object inSelectedNode)
 	{
 		fieldSelectedNode = inSelectedNode;
 		getSelectedNodes().clear();
-		if(inSelectedNode != null)
+		if (inSelectedNode != null)
 		{
 			getSelectedNodes().add(getId(inSelectedNode));
 			Object parent = getWebTree().getModel().getParent(inSelectedNode);
-			if( parent != null )
+			if (parent != null)
 			{
 				expandNode(parent);
 			}
-		}	
+		}
 
 	}
+
 	public String getUrlPostfix()
 	{
 		return fieldUrlPostfix;
 	}
+
 	public void setUrlPostfix(String inUrlPostfix)
 	{
 		fieldUrlPostfix = inUrlPostfix;
 	}
+
 	public Object getLastCutoffNode()
 	{
 		return fieldLastCutoffNode;
 	}
+
 	public void setLastCutoffNode(Object inLastCutoffNode)
 	{
 		fieldLastCutoffNode = inLastCutoffNode;
 	}
+
 	public boolean isShowPathInfo(Object inNode)
 	{
-		//inNode == /a/b/c.html
-		//Cutoff == /a
-		//First make sure it is not expanded
-	
+		// inNode == /a/b/c.html
+		// Cutoff == /a
+		// First make sure it is not expanded
+
 		Object container = inNode;
-		if( getWebTree().getModel().isLeaf(container) )
+		if (getWebTree().getModel().isLeaf(container))
 		{
 			container = getWebTree().getModel().getParent(container);
 		}
-		if( hasBeenExpanded(container))
+		if (hasBeenExpanded(container))
 		{
 			return false;
 		}
-		//See if we are below the cut off. If we are below or equal then show the node (return true)
-		if( fieldLastCutoffNode != null)
+		// See if we are below the cut off. If we are below or equal then show the node (return true)
+		if (fieldLastCutoffNode != null)
 		{
 			Object parent = inNode;
-			while( parent != null)
+			while (parent != null)
 			{
-				if( parent == fieldLastCutoffNode)
+				if (parent == fieldLastCutoffNode)
 				{
 					return true;
 				}
@@ -600,6 +635,6 @@ public abstract class BaseTreeRenderer implements TreeRenderer
 			}
 			return false;
 		}
-		return true; //Show it since it is not expanded
+		return true; // Show it since it is not expanded
 	}
 }
